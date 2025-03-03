7c9d9b644bc39b4975fc91f5c55fdc9402ae49bb
ch09830249 <ch09830249@yahoo.com.tw>
2025-03-04 01:07:22 +0800

    Modify: Update Interface intro

--- System_Verilog/Interface/Interface.sv
+++ System_Verilog/Interface/Interface.sv
@@ -1,3 +1,47 @@
+/*
+  An Interface is a way to encapsulate signals into a block. 
+  Also it becomes easier to connect with the DUT and other verification components.
+
+  EX: APB bus protocol signals
+      interface apb_if (input pclk);
+        logic [31:0]    paddr;
+        logic [31:0]    pwdata;
+        logic [31:0]    prdata;
+        logic           penable;
+        logic           pwrite;
+        logic           psel;
+      endinterface
+
+  Why are signals declared logic?
+    - Drive signals of this type via assign statements (wire) and in a procedural block (reg)
+    - 4-states
+
+  Syntax:
+    interface myBus (input clk);
+      logic [7:0]  data;
+      logic      enable;
+
+      // From TestBench perspective, 'data' is input and 'write' is output
+      modport TB  (input data, clk, output enable);       => modport is used to define signal directions
+
+      // From DUT perspective, 'data' is output and 'enable' is input
+      modport DUT (output data, input enable, clk);
+    endinterface
+
+  Advantage:
+    - Interfaces can contain tasks, functions, parameters, variables, functional coverage, and assertions. 
+        Monitor and record the transactions via the interface within this block
+    - Be easier to connect to design regardless of the number of ports 
+        //Before interface
+        dut dut0  (.data (data),
+                  .enable (enable),
+                  //  all other signals
+                  );
+
+        // With interface - higher level of abstraction possible
+        dut dut0  (busIf.DUT);
+*/
+
 module dut (myBus busIf);
   always @ (posedge busIf.clk)
     if (busIf.enable)
