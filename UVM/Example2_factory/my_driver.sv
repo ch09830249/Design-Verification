5480f1f617a3cc3a3547713c43667e833028fe22
Kenny Chang <ch09830249@yahoo.com.tw>
2025-04-03 13:59:43 +0800

    Modify: Update UVM factory example

--- UVM/Example2_factory/my_driver.sv
+++ UVM/Example2_factory/my_driver.sv
@@ -1,13 +1,13 @@
+/*
+    `uvm_component_utils(my_driver): 所有派生自 uvm_component 及其派生類別的類別都應該使用 uvm_component_utils 巨集註冊
+*/
 class my_driver extends uvm_driver;
-
-    `uvm_component_utils(my_driver)     // 將 my_driver 登記在 UVM 內部的一張表中
+    `uvm_component_utils(my_driver)                                         // 使用 factory 機制需將 my_driver 登記在 UVM 內部的一張表中
     function new(string name = "my_driver", uvm_component parent = null);
         super.new(name, parent);
         `uvm_info("my_driver", "new is called", UVM_LOW);
     endfunction
-
     extern virtual task main_phase(uvm_phase phase);
-
 endclass
 
 task my_driver::main_phase(uvm_phase phase);
@@ -16,12 +16,12 @@ task my_driver::main_phase(uvm_phase phase);
     top_tb.rx_dv <= 1'b0;
     while(!top_tb.rst_n)
         @(posedge top_tb.clk);
-    for(int i = 0; i < 256; i++) begin
-        @(posedge top_tb.clk);
-        top_tb.rxd <= $urandom_range(0, 255);
-        top_tb.rx_dv <= 1'b1;
-        `uvm_info("my_driver", "data is drived", UVM_LOW);
-    end
+        for(int i = 0; i < 256; i++) begin
+            @(posedge top_tb.clk);
+            top_tb.rxd <= $urandom_range(0, 255);
+            top_tb.rx_dv <= 1'b1;
+            `uvm_info("my_driver", "data is drived", UVM_LOW);
+        end
     @(posedge top_tb.clk);
     top_tb.rx_dv <= 1'b0;
 endtask
--- UVM/Example2_factory/top_tb.sv
+++ UVM/Example2_factory/top_tb.sv
@@ -1,8 +1,8 @@
 `timescale 1ns/1ps
-`include "uvm_macros.svh"       // 把 uvm_macros.svh 檔案透過 include 語句包含進來。這是 UVM 中的一個文件，裡面包含了眾多的 macro 定義，只需要包含一次
+`include "uvm_macros.svh"
 
-import uvm_pkg::*;              // import 語句將整個 uvm_pkg 導入驗證平台中。只有導入了這個函式庫，編譯器在編譯 my_driver.sv 檔時才會認識其中的 uvm_driver 等類別名稱
-`include "my_driver.sv"         // 把剛剛定義好的 driver 引入
+import uvm_pkg::*;
+`include "my_driver.sv"
 
 module top_tb;
 
@@ -23,6 +23,11 @@ module top_tb;
     );
 
     initial begin
+        // original code
+        // my_driver drv;
+        // drv = new("drv", null);
+        // drv.main_phase(null);
+        // $finish();
         run_test("my_driver");      // 改成這個
         /*
         一個 run_test 語句會建立一個 my_driver 的實例，並且會自動呼叫 my_driver 的 main_phase (要透過 uvm_component_utils 註冊過才行)
@@ -47,6 +52,6 @@ module top_tb;
 endmodule
 
 /*
-new is called
-main_phased is called       這裡因為沒有 objection 所以沒有 data is driven
+    new is called
+    main_phased is called       這裡因為沒有 objection 所以沒有 data is driven (沒有在 my_driver 中的 main_phase 去 raise objection)
 */
