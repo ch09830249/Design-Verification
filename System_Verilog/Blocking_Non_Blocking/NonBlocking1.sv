c2e7bb2f2651b691f22435ae35e7b6381a13cfc4
Kenny Chang <ch09830249@yahoo.com.tw>
2025-04-06 11:45:22 +0800

    Modify: Update Nonblocking sv

--- System_Verilog/Blocking_Non_Blocking/NonBlocking1.sv
+++ System_Verilog/Blocking_Non_Blocking/NonBlocking1.sv
@@ -31,26 +31,26 @@ endmodule
 |__ Spawn Block1 at #0ns: initial
 |      |___ Time #0ns : a <= 8'DA, is non-blocking so note value of RHS (8'hDA) and execute next step
 |      |___ Time #0ns : $display() is blocking, so execute this statement: But a hasn't received new values so a=8'hx
-|      |___ End of time-step : Assign captured value to variable a, and a is now 8'hDA    => timestep 0 ns 的最後 assign a value
+|      |___ End of time-step : Assign captured value to variable a, and a is now 8'hDA                                    => timestep 0 ns 的最後 assign a value
 |      |___ Wait until time advances by 10 time-units to #10ns
 |	
 |      |___ Time #10ns : b <= 8'F1, is non-blocking so note value of RHS (8'hF1) and execute next step
 |      |___ Time #10ns : $display() is blocking, so execute this statement. But b hasn't received new values so b=8'hx
 |	     |___ Time #10ns : c <= 8'30, is non-blocking so note value of RHS (8'h30) and execute next step
 |      |___ Time #10ns : $display() is blocking, so execute this statement. But c hasn't received new values so c=8'hx
-|      |___ End of time-step and initial block, assign captured values into variables b, c  => timestep 10 ns 的最後 assign b, c value
+|      |___ End of time-step and initial block, assign captured values into variables b, c                                => timestep 10 ns 的最後 assign b, c value
 |	
 |__ Spawn Block2 at #0ns: initial
 |      |___ Wait until time advances by 5 time-units to #5ns
 |	
 |      |___ Time #5ns : d <= 8'AA, is non-blocking so note value of RHS (8'hAA) and execute next step
 |      |___ Time #5ns : $display() is blocking, so execute this statement: But d hasn't received new values so d=8'hx
-|      |___ End of time-step : Assign captured value to variable d, and d is now 8'hAA  => timestep 5 ns 的最後 assign d value
+|      |___ End of time-step : Assign captured value to variable d, and d is now 8'hAA                                    => timestep 5 ns 的最後 assign d value
 |      |___ Wait until time advances by 5 time-units to #5ns
 |	
 |      |___ Time #10ns : e <= 8'55, is non-blocking so note value of RHS (8'h55) and execute next step
 |      |___ Time #10ns : $display() is blocking, so execute this statement. But e hasn't received new values so e=8'hx
-|      |___ End of time-step and initial block, assign captured values to variable e, and e is now 8'h55  => timestep 10 ns 的最後 assign e value
+|      |___ End of time-step and initial block, assign captured values to variable e, and e is now 8'h55                  => timestep 10 ns 的最後 assign e value
 |
 |__ End of simulation at #10ns
 */
