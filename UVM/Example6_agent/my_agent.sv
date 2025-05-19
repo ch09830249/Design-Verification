63f812624dc2fe57b1ca5b88609af1cac2a41c2f
ch09830249 <ch09830249@yahoo.com.tw>
2025-05-19 23:03:30 +0800

    Modify: Update UVM agent example code

--- UVM/Example6_agent/my_agent.sv
+++ UVM/Example6_agent/my_agent.sv
@@ -6,6 +6,18 @@ class my_agent extends uvm_agent;   // 所有的 agent 都要派生自 uvm_agent
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
+        /* 
+            透過 config table 取得當前 agent 的模式, 並指定給成員變數 is_active
+        */
+        uvm_config_db#(uvm_active_passive_enum)::get(this, "", "is_active", is_active);
+        /* 
+            is_active 的值預設為 UVM_ACTIVE，在這種模式下，是需要實例化 driver 的。
+            UVM_PASSIVE 模式在輸出埠上不需要驅動任何訊號，只需要監測訊號。在這種情況下，連接埠上是只需要 monitor 的，所以 driver 可以不用實例化。
+        */
+        if (is_active == UVM_ACTIVE) begin
+            drv = my_driver::type_id::create("drv", this);
+        end
+        mon = my_monitor::type_id::create("mon", this);
     endfunction
 
     extern virtual function void build_phase(uvm_phase phase);
@@ -17,18 +29,6 @@ endclass
 
 function void my_agent::build_phase(uvm_phase phase);
     super.build_phase(phase);
-    /* 
-        透過 config table 取得當前 agent 的模式, 並指定給成員變數 is_active
-    */
-    uvm_config_db#(uvm_active_passive_enum)::get(this, "", "is_active", is_active);
-    /* 
-        is_active 的值預設為 UVM_ACTIVE，在這種模式下，是需要實例化 driver 的。
-        UVM_PASSIVE 模式在輸出埠上不需要驅動任何訊號，只需要監測訊號。在這種情況下，連接埠上是只需要 monitor 的，所以 driver 可以不用實例化。
-    */
-    if (is_active == UVM_ACTIVE) begin
-        drv = my_driver::type_id::create("drv", this);
-    end
-    mon = my_monitor::type_id::create("mon", this);
 endfunction
 
 function void my_agent::connect_phase(uvm_phase phase);
