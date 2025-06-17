bdf2b77ede9c0c39e1aa9701b693efb6477818cf
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-06-17 15:41:35 +0800

    Modify: Update ImmediateAssertion.sv

--- System_Verilog/Assertion/ImmediateAssertion.sv
+++ System_Verilog/Assertion/ImmediateAssertion.sv
@@ -42,7 +42,7 @@ module tb;
       _if.empty <= $random;
       _if.full  <= $random;
       $strobe("[%0t] push=%0b full=%0b pop=%0b empty=%0b",
-              $time, _if.push, _if.full, _if.pop, _if.empty);
+              $time, _if.push, _if.full, _if.pop, _if.empty);  // 顯示某個時間點所有變數在模擬時間結束（即 time slot 的最後）時的值。
       @(posedge clk);
     end
     #10 $finish;
@@ -51,27 +51,29 @@ endmodule
 
 module my_des (my_if _if);
 
-  always @ (posedge _if.clk) begin
-    if (_if.push) begin
-    	// Immediate assertion and ensures that
-    	// fifo is not full when push is 1
-    	a_push: assert (!_if.full) begin
-      		$display("[PASS] push when fifo not full");
-    	end else begin
-      		$display("[FAIL] push when fifo full !");
-    	end
-  	end
-
-    if (_if.pop) begin
-    	// Immediate assertion to ensure that fifo is not
-    	// empty when pop is 1
-    	a_pop: assert (!_if.empty) begin
-      		$display ("[PASS] pop when fifo not empty");
-    	end else begin
-      		$display ("[FAIL] pop when fifo empty !");
-    	end
+    always @ (posedge _if.clk) begin
+        if (_if.push) begin
+            // Immediate assertion and ensures that
+            // fifo is not full when push is 1
+            a_push: assert (!_if.full) begin
+                $display("[PASS] push when fifo not full");
+            end 
+        else begin
+                $display("[FAIL] push when fifo full !");
+            end
+        end
+    
+        if (_if.pop) begin
+            // Immediate assertion to ensure that fifo is not
+            // empty when pop is 1
+            a_pop: assert (!_if.empty) begin
+                $display ("[PASS] pop when fifo not empty");
+            end 
+        else begin
+                $display ("[FAIL] pop when fifo empty !");
+            end
+        end
     end
-  end
 endmodule
 
 /*
