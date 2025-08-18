430166effd2f9f88f54b008424c2d0cf14c96410
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-18 10:56:31 +0800

    Update my_env.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example5_monitor/my_env.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example5_monitor/my_env.sv
@@ -6,10 +6,12 @@ class my_env extends uvm_env;
 
     function new(string name = "my_env", uvm_component parent);
         super.new(name, parent);
+        $display("my_env is new!!");
     endfunction
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        $display("my_env build phase!!");
         drv = my_driver::type_id::create("drv", this);
         // 一個用來監測 DUT 的輸入口，一個用來監測 DUT 的輸出口
         i_mon = my_monitor::type_id::create("i_mon", this);     // 當完成 monitor 的定義後，可以在 env 中進行實例化
