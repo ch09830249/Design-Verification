24f1814ddd2d7ad797be5a3b595c150cdbf28ae1
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-21 20:11:51 +0800

    Update my_env.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example8_scoreboard/my_env.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example8_scoreboard/my_env.sv
@@ -1,3 +1,6 @@
+`include "my_agent.sv"
+`include "my_model.sv"
+`include "my_scoreboard.sv"
 class my_env extends uvm_env;
 
     my_agent i_agt;
@@ -13,24 +16,26 @@ class my_env extends uvm_env;
 
     function new(string name = "my_env", uvm_component parent);
         super.new(name, parent);
+        `uvm_info("my_env", "my_envs is new", UVM_LOW);
     endfunction
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        `uvm_info("my_env", "my_env build phase!!", UVM_LOW);
         uvm_config_db#(uvm_active_passive_enum)::set(this, "i_agt", "is_active", UVM_ACTIVE);
         uvm_config_db#(uvm_active_passive_enum)::set(this, "o_agt", "is_active", UVM_PASSIVE);
         i_agt = my_agent::type_id::create("i_agt", this);
         o_agt = my_agent::type_id::create("o_agt", this);
         mdl   = my_model::type_id::create("mdl", this);
         // 實例化 scoreboard
-        scb   = my_model::type_id::create("scb", this);
+        scb   = my_scoreboard::type_id::create("scb", this);
         agt_mdl_fifo = new("agt_mdl_fifo", this);
         // 實例化 fifo
         mdl_scb_fifo = new("mdl_scb_fifo", this);
         agt_scb_fifo = new("agt_scb_fifo", this);
     endfunction
 
-    function void my_env::connect_phase(uvm_phase phase);
+    function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
         i_agt.ap.connect(agt_mdl_fifo.analysis_export);
         mdl.port.connect(agt_mdl_fifo.blocking_get_export);
@@ -43,4 +48,5 @@ class my_env extends uvm_env;
     endfunction
 
     `uvm_component_utils(my_env)
+
 endclass
