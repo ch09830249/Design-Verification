6e89bdc12de5362499f479d280ee6590738db92f
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-21 21:06:19 +0800

    Update my_transaction.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example9_field_automation/my_transaction.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example9_field_automation/my_transaction.sv
@@ -7,7 +7,7 @@ class my_transaction extends uvm_sequence_item;
     rand bit[31:0] crc;
 
     constraint pload_cons {
-        pload.size >= 46;
+        pload.size >= 20;
         pload.size <= 1500;
     }
 
@@ -16,38 +16,19 @@ class my_transaction extends uvm_sequence_item;
     endfunction
 
     function void post_randomize();
-        crc = calc_crc;
+        crc = calc_crc();
     endfunction
-
-    // `uvm_object_utils(my_transaction)
-
-    /*
-        使用 uvm_object_utils_begin 和 uvm_object_utils_end 來實作 my_transaction 的 factory 註冊，
-        在這兩個宏中間，使用 uvm_field 巨集註冊所有字段。 uvm_field 系列巨集隨著 transaction 成員變數的不同而不同，
-
-        使用上述巨集註冊之後，可以直接呼叫 copy、compare、print 等函數，而無需自己定義。這大大簡化了驗證平台的搭建，提高了效率
-    */
-    `uvm_object_utils_begin(my_transaction)
-        `uvm_field_int(dmac, UVM_ALL_ON)            // 針對 bit 類型的 uvm_field_int
-        `uvm_field_int(smac, UVM_ALL_ON)
-        `uvm_field_int(ether_type, UVM_ALL_ON)
-        `uvm_field_array_int(pload, UVM_ALL_ON)     // 針對 byte 型別動態陣列的 uvm_field_array_int
-        `uvm_field_int(crc, UVM_ALL_ON)
-    `uvm_object_utils_end
-
-    function new(string name = "my_transaction");
-        super.new(name);
-    endfunction
-
-    // 以下 my_copy, my_compare, my_print 改透過 field automation 實作
-    // function void my_print();
-    //     $display("dmac = %0h", dmac);
-    //     $display("smac = %0h", smac);
-    //     $display("ether_type = %0h", ether_type);
-    //     for(int i = 0; i < pload.size; i++) begin
+    
+    // function void my_print(string name);
+    //     $display("======================= Print Transaction (%s)=======================", name);
+    //     $display("dmac: %0h", dmac);
+    //     $display("smac: %0h", smac);
+    //     $display("ether_type: %0h", ether_type);
+    //     foreach (pload[i]) begin
     //         $display("pload[%0d] = %0h", i, pload[i]);
     //     end
-    //     $display("crc = %0h", crc);
+    //     $display("crc: %0h", crc);
+    //     $display("==============================================");
     // endfunction
 
     // function void my_copy(my_transaction tr);
@@ -81,4 +62,25 @@ class my_transaction extends uvm_sequence_item;
     //         end
     //     return result;
     // endfunction
+
+    // `uvm_object_utils(my_transaction)
+
+    /*
+        使用 uvm_object_utils_begin 和 uvm_object_utils_end 來實作 my_transaction 的 factory 註冊，
+        在這兩個宏中間，使用 uvm_field 巨集註冊所有字段。 uvm_field 系列巨集隨著 transaction 成員變數的不同而不同，
+
+        使用上述巨集註冊之後，可以直接呼叫 copy、compare、print 等函數，而無需自己定義。這大大簡化了驗證平台的搭建，提高了效率
+    */
+    `uvm_object_utils_begin(my_transaction)
+        `uvm_field_int(dmac, UVM_ALL_ON)            // 針對 bit 類型的 uvm_field_int
+        `uvm_field_int(smac, UVM_ALL_ON)
+        `uvm_field_int(ether_type, UVM_ALL_ON)
+        `uvm_field_array_int(pload, UVM_ALL_ON)     // 針對 byte 型別動態陣列的 uvm_field_array_int
+        `uvm_field_int(crc, UVM_ALL_ON)
+    `uvm_object_utils_end
+
+    function new(string name = "my_transaction");
+        super.new(name);
+    endfunction
+
 endclass
