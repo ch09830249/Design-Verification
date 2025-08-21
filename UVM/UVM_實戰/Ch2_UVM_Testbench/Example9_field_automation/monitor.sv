e7211a424343d6b886d42357881ee694904d2685
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-21 21:04:21 +0800

    Update monitor.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example9_field_automation/monitor.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example9_field_automation/monitor.sv
@@ -1,6 +1,6 @@
 class my_monitor extends uvm_monitor;
-
     virtual my_if vif;
+    byte payload_q[$];
     uvm_analysis_port #(my_transaction) ap;
 
     `uvm_component_utils(my_monitor)
@@ -13,6 +13,8 @@ class my_monitor extends uvm_monitor;
         super.build_phase(phase);
         if (!uvm_config_db#(virtual my_if)::get(this, "", "vif", vif))
             `uvm_fatal("my_monitor", "virtual interface must be set for vif!!!")
+        else
+            `uvm_info("my_monitor", "get virtual interface vif successfully!!!", UVM_LOW);
         ap = new("ap", this);
     endfunction
 
@@ -23,16 +25,20 @@ endclass
 
 task my_monitor::main_phase(uvm_phase phase);
     my_transaction tr;
+    `uvm_info("my_monitor", "main_phase is called", UVM_LOW);
     while (1) begin
         tr = new("tr");
         collect_one_pkt(tr);
         ap.write(tr);
     end
+    `uvm_info("my_monitor", "main_phase is ended", UVM_LOW);
 endtask
 
 task my_monitor::collect_one_pkt(my_transaction tr);
     bit[7:0] data_q[$];
     int psize;
+    int data_size;
+    byte unsigned data_array[];
 
     while (1) begin
         @(posedge vif.clk);
@@ -61,24 +67,34 @@ task my_monitor::collect_one_pkt(my_transaction tr);
         data_array[i] = data_q[i];
     end
     // 由於 tr 中的 pload 是一個動態數組，所以需要在調用 unpack_bytes 之前就指定其大小，這樣 unpack_bytes 函數才能正常運作。
-    tr.pload = new[data_size - 18]; //da sa, e_type, crc
+    tr.pload = new[data_size - 18]; //扣掉 da sa, e_type, crc
     // 直接 unpack 到該 transaction
     data_size = tr.unpack_bytes(data_array) / 8;
 
-    // 以下將 packet 轉 Byte 資料流的 code 改成 field automation 方式實作
     // // Pop dmac
     // for (int i = 0; i < 6; i++) begin
     //     tr.dmac = {tr.dmac[39:0], data_q.pop_front()};
     // end
 
     // // Pop smac
-    // …
+    // for (int i = 0; i < 6; i++) begin
+    //     tr.smac = {tr.smac[39:0], data_q.pop_front()};
+    // end
 
     // // Pop ether_type
-    // …
+    // for (int i = 0; i < 2; i++) begin
+    //     tr.ether_type = {tr.ether_type[7:0], data_q.pop_front()};
+    // end
 
     // // Pop payload
-    // …
+    // payload_q = {};
+    // for (int i = 0; i < 20; i++) begin
+    //     payload_q.push_back(data_q.pop_front());
+    // end
+    // tr.pload = new[20];
+    // for (int i = 0; i < payload_q.size(); i++) begin
+    //     tr.pload[i] = payload_q[i];
+    // end
 
     // // Pop crc
     // for (int i = 0; i < 4; i++) begin
@@ -86,6 +102,6 @@ task my_monitor::collect_one_pkt(my_transaction tr);
     // end
 
     `uvm_info("my_monitor", "end collect one pkt, print it:", UVM_LOW);
-    // tr.my_print();
+    // tr.my_print("my_monitor");
     tr.print();
 endtask
