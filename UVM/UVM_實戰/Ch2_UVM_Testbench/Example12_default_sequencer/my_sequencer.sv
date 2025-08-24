ef0625db5d4527705381c96c603d298c92504e40
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-24 21:50:01 +0800

    Update my_sequencer.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example12_default_sequencer/my_sequencer.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example12_default_sequencer/my_sequencer.sv
@@ -3,16 +3,17 @@
     sequence 是在 my_env 或是在 my_sequencer 的 main_phase 中手動啟動的，但是在實際應用中，
     使用最多的還是透過 default_sequence 的方式啟動 sequence。
 */
+`include "my_sequence.sv"
 class my_sequencer extends uvm_sequencer #(my_transaction);
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
     endfunction
 
-    task my_sequencer::main_phase(uvm_phase phase);
+    task main_phase(uvm_phase phase);
         // my_sequence seq;
         // phase.raise_objection(this);
-        // seq = my_sequence::type_id::create("seq");   // 手動啟動的部分
+        // seq = my_sequence::type_id::create("seq");
         // seq.start(this);
         // phase.drop_objection(this);
     endtask
