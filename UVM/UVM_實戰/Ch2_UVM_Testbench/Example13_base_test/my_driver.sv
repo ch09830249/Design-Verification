d3d24b5ca56828454e36a77ee5834fcdd63c9875
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-25 09:24:22 +0800

    Update my_driver.sv

--- UVM/UVM_實戰/Ch2_UVM_Testbench/Example13_base_test/my_driver.sv
+++ UVM/UVM_實戰/Ch2_UVM_Testbench/Example13_base_test/my_driver.sv
@@ -1,25 +1,28 @@
+`include "my_transaction.sv"
 class my_driver extends uvm_driver#(my_transaction);
     virtual my_if vif;
-
+    virtual my_if vif2;
     `uvm_component_utils(my_driver)
+
     function new(string name = "my_driver", uvm_component parent = null);
         super.new(name, parent);
         `uvm_info("my_driver", "new is called", UVM_LOW);
     endfunction
 
     extern virtual task main_phase(uvm_phase phase);
-    extern virutal task drive_one_pkt(my_transaction tr);
+    extern virtual task drive_one_pkt(my_transaction tr);
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         `uvm_info("my_driver", "build_phase is called", UVM_LOW);
         if(!uvm_config_db#(virtual my_if)::get(this, "", "vif", vif)) 
             `uvm_fatal("my_driver", "virtual interface must be set for vif!!!")
+        else
+            `uvm_info("my_driver", "get virtual interface vif successfully!!!", UVM_LOW)
     endfunction
 endclass
 
 task my_driver::main_phase(uvm_phase phase);
-    phase.raise_objection(this);
     vif.data <= 8'b0;
     vif.valid <= 1'b0;
     while(!vif.rst_n)
@@ -30,22 +33,20 @@ task my_driver::main_phase(uvm_phase phase);
         seq_item_port.item_done();
     end
     repeat(5) @(posedge vif.clk);
-    phase.drop_objection(this);
 endtask
 
 task my_driver::drive_one_pkt(my_transaction tr);
-    bit [47:0] tmp_data;
-    bit [7:0] data_q[$];
-
+    byte unsigned data_q[];
+    int data_size;
+    
     data_size = tr.pack_bytes(data_q) / 8;
-
     `uvm_info("my_driver", "begin to drive one pkt", UVM_LOW);
-    repeat(3) @(posedge vif.clk);
-
-    while (data_q.size() > 0) begin
+    tr.print();
+    repeat(5) @(posedge vif.clk);
+    for ( int i = 0; i < data_size; i++ ) begin
         @(posedge vif.clk);
         vif.valid <= 1'b1;
-        vif.data <= data_q.pop_front();
+        vif.data <= data_q[i];
     end
 
     @(posedge vif.clk);
