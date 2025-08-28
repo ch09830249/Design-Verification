1b568ec20d175b7d879750d27d3d28867b95d517
ch09830249 <ch09830249@yahoo.com.tw>
2025-08-29 01:20:42 +0800

    Add AHB simple VIP only for write

---
+++ AMBA/AHB/AHB_write/module/apb_if.sv
@@ -0,0 +1,10 @@
+interface apb_if(input logic PCLK, input logic PRESETn);
+
+  logic [31:0] PADDR;
+  logic        PWRITE;
+  logic        PENABLE;
+  logic        PSEL;
+  logic [31:0] PWDATA;
+  logic        PREADY;
+
+endinterface
