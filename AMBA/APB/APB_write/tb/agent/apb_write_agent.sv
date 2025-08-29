18208dc63c741482096d213630446c11f5cce024
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 00:36:52 +0800

    Update apb_write_agent.sv

--- AMBA/APB/APB_write/tb/agent/apb_write_agent.sv
+++ AMBA/APB/APB_write/tb/agent/apb_write_agent.sv
@@ -1,13 +1,20 @@
-class ahb_agent extends uvm_agent;
-  `uvm_component_utils(ahb_agent)
+class apb_write_agent extends uvm_agent;
+  `uvm_component_utils(apb_write_agent)
 
-  ahb_driver driver;
+  apb_write_driver driver;
+  uvm_sequencer #(apb_write_transaction) sequencer;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "apb_write_agent", uvm_component parent = null);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    driver = ahb_driver::type_id::create("driver", this);
+    super.build_phase(phase);
+    sequencer = uvm_sequencer #(apb_write_transaction)::type_id::create("sequencer", this);
+    driver = apb_write_driver::type_id::create("driver", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
   endfunction
 endclass
