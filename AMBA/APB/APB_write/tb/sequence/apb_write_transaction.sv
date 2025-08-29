68f97bd605db0d9784f5009727ccfb10daafc9d4
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 00:39:29 +0800

    Update apb_write_transaction.sv

--- AMBA/APB/APB_write/tb/sequence/apb_write_transaction.sv
+++ AMBA/APB/APB_write/tb/sequence/apb_write_transaction.sv
@@ -1,10 +1,13 @@
-class ahb_seq_item extends uvm_sequence_item;
+class apb_write_transaction extends uvm_sequence_item;
   rand bit [31:0] addr;
   rand bit [31:0] data;
 
-  `uvm_object_utils(ahb_seq_item)
+  `uvm_object_utils_begin(apb_write_transaction)
+        `uvm_field_int(addr, UVM_ALL_ON)
+        `uvm_field_int(data, UVM_ALL_ON)
+  `uvm_object_utils_end
 
-  function new(string name = "ahb_seq_item");
+  function new(string name = "apb_write_transaction");
     super.new(name);
   endfunction
 endclass
