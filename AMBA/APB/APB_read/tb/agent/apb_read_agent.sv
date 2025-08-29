1cf5a4abbaf4c547261ada6fa8fe9d2bfff9478a
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-29 23:40:53 +0800

    Update apb_read_agent.sv

--- AMBA/APB/APB_read/tb/agent/apb_read_agent.sv
+++ AMBA/APB/APB_read/tb/agent/apb_read_agent.sv
@@ -1,13 +1,20 @@
-class ahb_agent extends uvm_agent;
-  `uvm_component_utils(ahb_agent)
+class apb_read_agent extends uvm_agent;
+  `uvm_component_utils(apb_read_agent)
 
-  ahb_driver driver;
+  apb_read_driver driver;
+  uvm_sequencer #(apb_read_transaction) sequencer;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "apb_read_agent", uvm_component parent = null);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    driver = ahb_driver::type_id::create("driver", this);
+    super.build_phase(phase);
+    sequencer = uvm_sequencer #(apb_read_transaction)::type_id::create("sequencer", this);
+    driver = apb_read_driver::type_id::create("driver", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
   endfunction
 endclass
