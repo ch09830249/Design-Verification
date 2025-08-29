25e7e2d28cd447ca7275b58e928701c1d733357a
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-29 23:46:40 +0800

    Update apb_read_sequence.sv

--- AMBA/APB/APB_read/tb/sequence/apb_read_sequence.sv
+++ AMBA/APB/APB_read/tb/sequence/apb_read_sequence.sv
@@ -1,17 +1,16 @@
-class apb_write_sequence extends uvm_sequence #(ahb_transaction);
-  `uvm_object_utils(apb_write_sequence)
+class apb_read_sequence extends uvm_sequence #(apb_read_transaction);
+  `uvm_object_utils(apb_read_sequence)
 
-  function new(string name = "apb_write_sequence");
+  function new(string name = "apb_read_sequence");
     super.new(name);
   endfunction
 
   task body();
-    ahb_transaction tx;
-    repeat (5) begin
-      tx = ahb_transaction::type_id::create("tx");
-      assert(tx.randomize() with {
-        addr inside {[32'h0000_0000 : 32'h0000_00FF]};
-      });
+    apb_read_transaction tx;
+    for(int i = 0; i < 4; i++) begin                    // 發 4 筆 APB write
+      tx = apb_read_transaction::type_id::create("tx");
+      tx.addr = 32'h1000 + i * 4;                       // addr: 32'h1000, 32'h1004, 32'h1008, 32'h100C...
+      tx.data = 0;
       start_item(tx);
       finish_item(tx);
     end
