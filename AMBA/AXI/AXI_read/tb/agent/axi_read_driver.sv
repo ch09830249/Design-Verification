1235667a90fdca5e54198fe2d2cfe1f3d0410a65
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:14:19 +0800

    Update axi_read_driver.sv

--- AMBA/AXI/AXI_read/tb/agent/axi_read_driver.sv
+++ AMBA/AXI/AXI_read/tb/agent/axi_read_driver.sv
@@ -1,37 +1,73 @@
-class ahb_driver extends uvm_driver #(ahb_transaction);
-  `uvm_component_utils(ahb_driver)
+class axi_read_driver extends uvm_driver #(axi_read_transaction);
+  `uvm_component_utils(axi_read_driver)
 
-  virtual ahb_if vif;
+  virtual axi_read_if vif;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "axi_read_driver", uvm_component parent = null);  
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    if (!uvm_config_db#(virtual ahb_if)::get(this, "", "vif", vif))
-      `uvm_fatal("NOVIF", "No virtual interface specified for ahb_driver")
+    super.build_phase(phase);
+    if (!uvm_config_db #(virtual axi_read_if)::get(this, "", "vif", vif)) begin
+      `uvm_fatal(get_type_name(), "Virtual interface not found!")
+    end
   endfunction
 
-  task run_phase(uvm_phase phase);
-    ahb_transaction tx;
+  virtual task run_phase(uvm_phase phase);
+
+    axi_read_transaction tr;
+  
     forever begin
-      seq_item_port.get_next_item(tx);
+      seq_item_port.get_next_item(tr);
 
-      // Setup phase
-      vif.HSEL   <= 1;
-      vif.HWRITE <= 1;
-      vif.HADDR  <= tx.addr;
-      vif.HWDATA <= tx.data;
-      vif.HTRANS <= 2'b10; // NONSEQ
+      `uvm_info(get_type_name(), "before read", UVM_MEDIUM)
+      tr.print();
 
-      // Wait for HREADY
-      do @(posedge vif.HCLK); while (!vif.HREADYOUT);
+      // Read Address => assign ARADDR && ARVALID
+      @(posedge vif.ACLK);
+      vif.ARADDR  <= tr.araddr;                       // 塞入 read address 並且舉 ARVALID
+      vif.ARVALID <= 1;
 
-      // Done
-      vif.HSEL   <= 0;
-      vif.HTRANS <= 2'b00;
+      // Wait for data & Response
+      do @(posedge vif.ACLK); while (!vif.ARREADY);   // 等 module 收完 read address (ARREADY 被舉起)
+      vif.ARVALID <= 0;                               // 將 ARVALID 放下並且舉起 RREADY 準備收 module 的 data
+      vif.RREADY <= 1;
 
+      // Read Data & RSP
+      do @(posedge vif.ACLK); while (!vif.RVALID);    // 直到 RVALID 舉起代表 data 可以收了
+      tr.rdata = vif.RDATA;                           // 收 DATA 和 RSP 之後 放下 RREADY
+      tr.rresp = vif.RRESP;
+      vif.RREADY <= 0;
+
+      `uvm_info(get_type_name(), "after read", UVM_MEDIUM)
+      tr.print();
+      
       seq_item_port.item_done();
     end
   endtask
 endclass
+
+/*
+ task drive_read(axi_read_xtn xtn);
+    // AR channel
+    axi_vif.arvalid <= 1;
+    axi_vif.araddr  <= xtn.araddr;
+    axi_vif.arid    <= xtn.arid;
+    axi_vif.arlen   <= xtn.arlen;
+    axi_vif.arsize  <= xtn.arsize;
+    axi_vif.arburst <= xtn.arburst;
+
+    wait (axi_vif.arready == 1);
+    axi_vif.arvalid <= 0;
+
+    // R channel - receive data
+    for (int i = 0; i <= xtn.arlen; i++) begin
+      wait (axi_vif.rvalid == 1);
+      xtn.rdata[i] = axi_vif.rdata;
+      axi_vif.rready <= 1;
+      @(posedge axi_vif.clk);
+      axi_vif.rready <= 0;
+    end
+  endtask
+  */
