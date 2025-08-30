d6d510bce2413978e755a9119aeaa1b8a9b816a5
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:19:23 +0800

    Update axi_read_if.sv

--- AMBA/AXI/AXI_read/tb/interface/axi_read_if.sv
+++ AMBA/AXI/AXI_read/tb/interface/axi_read_if.sv
@@ -1,11 +1,9 @@
-interface ahb_if(input logic HCLK, input logic HRESETn);
-
-  logic [31:0] HADDR;
-  logic [31:0] HWDATA;
-  logic        HWRITE;
-  logic [1:0]  HTRANS;
-  logic        HREADY;
-  logic        HREADYOUT;
-  logic        HSEL;
-
+interface axi_read_if(input logic ACLK, input logic ARESETn);
+    logic [31:0] ARADDR;
+    logic        ARVALID;
+    logic        ARREADY;
+    logic [31:0] RDATA;
+    logic [1:0]  RRESP;
+    logic        RVALID;
+    logic        RREADY;
 endinterface
