2c6d8b689e4b10451b916473944f541c9649454a
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:53:40 +0800

    Update axi_write_top.sv

--- AMBA/AXI/AXI_write/tb/axi_write_top.sv
+++ AMBA/AXI/AXI_write/tb/axi_write_top.sv
@@ -1,42 +1,43 @@
-module top;
+`timescale 1ns/1ps
+import uvm_pkg::*;
+`include "uvm_macros.svh"
 
-  logic HCLK;
-  logic HRESETn;
+module axi_write_top;
 
-  // Instantiate interface
-  ahb_if ahb_if_inst(.HCLK(HCLK), .HRESETn(HRESETn));
+  logic clk = 0;
+  logic rst_n = 0;
 
-  // Clock generation
-  initial begin
-    HCLK = 0;
-    forever #5 HCLK = ~HCLK;
-  end
+  always #5 clk = ~clk;
 
-  // Reset generation
-  initial begin
-    HRESETn = 0;
-    #20 HRESETn = 1;
-  end
+  axi_write_if axi_write_if(.ACLK(clk), .ARESETn(rst_n));
 
-  // DUT instance
-  ahb_write_slavel dut (
-    .HCLK      (HCLK),
-    .HRESETn   (HRESETn),
-    .HSEL      (ahb_if_inst.HSEL),
-    .HWRITE    (ahb_if_inst.HWRITE),
-    .HTRANS    (ahb_if_inst.HTRANS),
-    .HADDR     (ahb_if_inst.HADDR),
-    .HWDATA    (ahb_if_inst.HWDATA),
-    .HREADYOUT (ahb_if_inst.HREADYOUT)
+  axi_write_slave dut (
+    .ACLK(clk),
+    .ARESETn(rst_n),
+    .AWADDR(axi_write_if.AWADDR),
+    .AWVALID(axi_write_if.AWVALID),
+    .AWREADY(axi_write_if.AWREADY),
+    .WDATA(axi_write_if.WDATA),
+    .WVALID(axi_write_if.WVALID),
+    .WREADY(axi_write_if.WREADY),
+    .BRESP(axi_write_if.BRESP),
+    .BVALID(axi_write_if.BVALID),
+    .BREADY(axi_write_if.BREADY)
   );
 
-  // Connect VIP to interface
   initial begin
-    uvm_config_db#(virtual ahb_if)::set(null, "*", "vif", ahb_if_inst);
+    rst_n = 0;
+    #20                 // 20 ns 後 not rst 舉起來 => 開始運作
+    rst_n = 1;
   end
 
   initial begin
-    run_test("ahb_test");
+    uvm_config_db#(virtual axi_write_if)::set(null, "*", "vif", axi_write_if);
+    run_test("axi_write_test");
   end
 
+  initial begin
+    $shm_open("waves.shm");
+    $shm_probe("AS");
+  end
 endmodule
