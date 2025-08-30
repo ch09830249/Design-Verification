3aa8891c6da7a8495d84ec592f96c0acad3a946d
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:19:04 +0800

    Update axi_read_env.sv

--- AMBA/AXI/AXI_read/tb/env/axi_read_env.sv
+++ AMBA/AXI/AXI_read/tb/env/axi_read_env.sv
@@ -1,13 +1,15 @@
-class ahb_env extends uvm_env;
-  `uvm_component_utils(ahb_env)
+class axi_read_env extends uvm_env;
 
-  ahb_agent agent;
+  `uvm_component_utils(axi_read_env)
 
-  function new(string name, uvm_component parent);
+  axi_read_agent read_agent;
+
+  function new(string name = "axi_read_env", uvm_component parent = null);  
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    agent = ahb_agent::type_id::create("agent", this);
+    super.build_phase(phase);
+    read_agent = axi_read_agent::type_id::create("read_agent", this);
   endfunction
 endclass
