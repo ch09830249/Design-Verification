bb201362f6dec90e7a38e30fc52914c375c0d019
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:51:05 +0800

    Update axi_write_driver.sv

--- AMBA/AXI/AXI_write/tb/agent/axi_write_driver.sv
+++ AMBA/AXI/AXI_write/tb/agent/axi_write_driver.sv
@@ -1,35 +1,46 @@
-class ahb_driver extends uvm_driver #(ahb_transaction);
-  `uvm_component_utils(ahb_driver)
+class axi_write_driver extends uvm_driver #(axi_write_transaction);
+  `uvm_component_utils(axi_write_driver)
 
-  virtual ahb_if vif;
+  virtual axi_write_if vif;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "axi_write_driver", uvm_component parent = null); 
     super.new(name, parent);
   endfunction
 
-  function void build_phase(uvm_phase phase);
-    if (!uvm_config_db#(virtual ahb_if)::get(this, "", "vif", vif))
-      `uvm_fatal("NOVIF", "No virtual interface specified for ahb_driver")
+  virtual function void build_phase(uvm_phase phase);
+    if (!uvm_config_db#(virtual axi_write_if)::get(this, "", "vif", vif))   // 取得從 top 傳來的 virtual interface
+      `uvm_fatal("DRV", "No virtual interface bound to driver");
   endfunction
 
-  task run_phase(uvm_phase phase);
-    ahb_transaction tx;
+  virtual task run_phase(uvm_phase phase);
+    axi_write_transaction tr;
+
     forever begin
-      seq_item_port.get_next_item(tx);
+      seq_item_port.get_next_item(tr);
 
-      // Setup phase
-      vif.HSEL   <= 1;
-      vif.HWRITE <= 1;
-      vif.HADDR  <= tx.addr;
-      vif.HWDATA <= tx.data;
-      vif.HTRANS <= 2'b10; // NONSEQ
+      // Write Address => assign AWADDR && AWVALID
+      @(posedge vif.ACLK);
+      vif.AWADDR  <= tr.addr;                         // 塞 tr 的 write address
+      vif.AWVALID <= 1;                               // 舉 AWVALID 表示 module 可以取 write address
 
-      // Wait for HREADY
-      do @(posedge vif.HCLK); while (!vif.HREADYOUT);
+      // Write data
+      wait (vif.AWREADY == 1);                        // 直到 AWREADY 為 1
+      vif.AWVALID <= 0;                               // 將 AWVALID 放下, 準備塞 data
+      vif.WDATA  <= tr.data;                          // 塞 tr 的 write data
+      vif.WVALID <= 1;                                // 舉 WVALID 表示可以取 write data
+      
+      // Write data done
+      wait (vif.WREADY == 1);                         // 直到 WREADY 為 1
+      vif.WVALID <= 0;                                // 將 WVALID 放下, 準備等待 write data 有沒有成功
 
-      // Done
-      vif.HSEL   <= 0;
-      vif.HTRANS <= 2'b00;
+      // Wait for Response
+      wait (vif.BVALID == 1);                         // 直到 BVALID 為 1
+      `uvm_info("DRV", $sformatf("BRESP: %0d", vif.BRESP), UVM_LOW);
+      vif.BREADY <= 1;                                // 看完狀態把 BREADY 舉起代表我看完了
+
+      // Reset BREADY
+      @(posedge vif.ACLK);
+      vif.BREADY <= 0;                                // 等一個 clk 之後回復 BREADY 狀態
 
       seq_item_port.item_done();
     end
