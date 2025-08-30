68e2871c5b596eb670d4757fe143e4a8a1179418
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:25:27 +0800

    Update axi_read_flist.f

--- AMBA/AXI/AXI_read/tb/axi_read_flist.f
+++ AMBA/AXI/AXI_read/tb/axi_read_flist.f
@@ -1,42 +1,15 @@
-module top;
+# === RTL and Interface
+./rtl/axi_read_slave.sv
 
-  logic HCLK;
-  logic HRESETn;
+# === UVM TB Components
+./tb/interface/axi_read_if.sv
+./tb/sequence/axi_read_transaction.sv
+./tb/sequence/axi_read_sequence.sv
+./tb/agent/axi_read_monitor.sv
+./tb/agent/axi_read_driver.sv
+./tb/agent/axi_read_agent.sv
+./tb/env/axi_read_env.sv
+./tb/test/axi_read_test.sv
 
-  // Instantiate interface
-  ahb_if ahb_if_inst(.HCLK(HCLK), .HRESETn(HRESETn));
-
-  // Clock generation
-  initial begin
-    HCLK = 0;
-    forever #5 HCLK = ~HCLK;
-  end
-
-  // Reset generation
-  initial begin
-    HRESETn = 0;
-    #20 HRESETn = 1;
-  end
-
-  // DUT instance
-  ahb_write_slavel dut (
-    .HCLK      (HCLK),
-    .HRESETn   (HRESETn),
-    .HSEL      (ahb_if_inst.HSEL),
-    .HWRITE    (ahb_if_inst.HWRITE),
-    .HTRANS    (ahb_if_inst.HTRANS),
-    .HADDR     (ahb_if_inst.HADDR),
-    .HWDATA    (ahb_if_inst.HWDATA),
-    .HREADYOUT (ahb_if_inst.HREADYOUT)
-  );
-
-  // Connect VIP to interface
-  initial begin
-    uvm_config_db#(virtual ahb_if)::set(null, "*", "vif", ahb_if_inst);
-  end
-
-  initial begin
-    run_test("ahb_test");
-  end
-
-endmodule
+# === Top-level
+./tb/axi_read_top.sv
