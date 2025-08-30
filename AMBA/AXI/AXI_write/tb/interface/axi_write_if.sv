6b68b53717b85925da13aae61401c5b759f42605
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:51:50 +0800

    Update axi_write_if.sv

--- AMBA/AXI/AXI_write/tb/interface/axi_write_if.sv
+++ AMBA/AXI/AXI_write/tb/interface/axi_write_if.sv
@@ -1,11 +1,8 @@
-interface ahb_if(input logic HCLK, input logic HRESETn);
-
-  logic [31:0] HADDR;
-  logic [31:0] HWDATA;
-  logic        HWRITE;
-  logic [1:0]  HTRANS;
-  logic        HREADY;
-  logic        HREADYOUT;
-  logic        HSEL;
-
+interface axi_write_if(input logic ACLK, input logic ARESETn);
+  logic [31:0] AWADDR;
+  logic        AWVALID, AWREADY;
+  logic [31:0] WDATA;
+  logic        WVALID, WREADY;
+  logic [1:0]  BRESP;
+  logic        BVALID, BREADY;
 endinterface
