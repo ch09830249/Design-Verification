afcc017c3eb9c6edae976adaa2fbd06e4fe8e861
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 11:38:52 +0800

    Update axi_read_slave.sv

--- AMBA/AXI/AXI_read/rtl/axi_read_slave.sv
+++ AMBA/AXI/AXI_read/rtl/axi_read_slave.sv
@@ -1,28 +1,62 @@
-module ahb_write_slavel (
-  input  logic         HCLK,
-  input  logic         HRESETn,
-  input  logic         HSEL,
-  input  logic         HWRITE,
-  input  logic [1:0]   HTRANS,
-  input  logic [31:0]  HADDR,
-  input  logic [31:0]  HWDATA,
-  output logic         HREADYOUT
+module axi_read_slave #(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32
+)(
+    input  logic                 ACLK,
+    input  logic                 ARESETn,
+
+    input  logic [ADDR_WIDTH-1:0] ARADDR,
+    input  logic                  ARVALID,
+    output logic                  ARREADY,
+
+    output logic [DATA_WIDTH-1:0] RDATA,
+    output logic [1:0]            RRESP,
+    output logic                  RVALID,
+    input  logic                  RREADY
 );
 
-  logic [31:0] mem [0:255];
+    typedef enum logic [1:0] {
+        R_IDLE, R_ADDR_ACCEPTED, R_RESP_SENT
+    } r_state_t;
 
-  assign HREADYOUT = 1;
+    r_state_t rstate;
 
-  always_ff @(posedge HCLK or negedge HRESETn) begin
-    if (!HRESETn) begin
-      int i;
-      for (i = 0; i < 256; i++)
-        mem[i] <= 0;
-    end else begin
-      if (HSEL && HWRITE && HTRANS[1]) begin
-        mem[HADDR[9:2]] <= HWDATA;
-      end
+    always_ff @(posedge ACLK or negedge ARESETn) begin
+        if (!ARESETn) begin
+            ARREADY <= 0;
+            RVALID  <= 0;
+            RRESP   <= 2'b00;
+            RDATA   <= 0;
+            rstate  <= R_IDLE;
+        end 
+        else begin
+            case (rstate)
+                R_IDLE: begin
+                    if (ARVALID) begin
+                        ARREADY <= 1;
+                        rstate <= R_ADDR_ACCEPTED;
+                    end
+                end
+
+                R_ADDR_ACCEPTED: begin
+                    ARREADY <= 0;
+                    RVALID <= 1;
+                    RRESP  <= 2'b00;
+                    /*
+                        回傳資料為: 前 16 bits 為 ABCD, 後 16 bits 為 Read address bit 15~0
+                    */
+                    RDATA  <= {16'hABCD, ARADDR[15:0]};
+                    rstate <= R_RESP_SENT;
+                end
+
+                R_RESP_SENT: begin
+                    if (RREADY) begin
+                        RVALID <= 0;
+                        rstate <= R_IDLE;
+                    end
+                end
+            endcase
+        end
     end
-  end
 
 endmodule
