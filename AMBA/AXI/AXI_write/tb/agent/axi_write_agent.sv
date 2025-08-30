fc83133d64402cbb8d0451719a97ac3fe4dd6f22
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:32:04 +0800

    Update axi_write_agent.sv

--- AMBA/AXI/AXI_write/tb/agent/axi_write_agent.sv
+++ AMBA/AXI/AXI_write/tb/agent/axi_write_agent.sv
@@ -1,13 +1,20 @@
-class ahb_agent extends uvm_agent;
-  `uvm_component_utils(ahb_agent)
+class axi_write_agent extends uvm_agent;
+  `uvm_component_utils(axi_write_agent)
 
-  ahb_driver driver;
+  axi_write_driver driver;
+  uvm_sequencer #(axi_write_transaction) sequencer;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "axi_write_agent", uvm_component parent = null);  
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    driver = ahb_driver::type_id::create("driver", this);
+    super.build_phase(phase);
+    sequencer = uvm_sequencer #(axi_write_transaction)::type_id::create("sequencer", this);
+    driver    = axi_write_driver::type_id::create("driver", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);    // driver 和 sequencer port 連接 
   endfunction
 endclass
