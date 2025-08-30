8ef9dbe9025ca1e2304026ea1fa06ef6e2b8e852
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 11:39:39 +0800

    Update axi_read_agent.sv

--- AMBA/AXI/AXI_read/tb/agent/axi_read_agent.sv
+++ AMBA/AXI/AXI_read/tb/agent/axi_read_agent.sv
@@ -1,13 +1,22 @@
-class ahb_agent extends uvm_agent;
-  `uvm_component_utils(ahb_agent)
+class axi_read_agent extends uvm_agent;
+  `uvm_component_utils(axi_read_agent)
 
-  ahb_driver driver;
+  axi_read_driver driver;
+  axi_read_monitor monitor;
+  uvm_sequencer #(axi_read_transaction) sequencer;
 
-  function new(string name, uvm_component parent);
+  function new(string name = "axi_read_agent", uvm_component parent = null);  
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    driver = ahb_driver::type_id::create("driver", this);
+    super.build_phase(phase);
+    sequencer = uvm_sequencer #(axi_read_transaction)::type_id::create("sequencer", this);
+    driver    = axi_read_driver::type_id::create("driver", this);
+    monitor   = axi_read_monitor::type_id::create("monitor", this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);    
   endfunction
 endclass
