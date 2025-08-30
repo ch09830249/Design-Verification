ce2f3f9b14ae6948a86030851c5af3e0691a666f
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:52:38 +0800

    Update axi_write_transaction.sv

--- AMBA/AXI/AXI_write/tb/sequence/axi_write_transaction.sv
+++ AMBA/AXI/AXI_write/tb/sequence/axi_write_transaction.sv
@@ -1,10 +1,13 @@
-class ahb_seq_item extends uvm_sequence_item;
+class axi_write_transaction extends uvm_sequence_item;
   rand bit [31:0] addr;
   rand bit [31:0] data;
 
-  `uvm_object_utils(ahb_seq_item)
+  `uvm_object_utils_begin(axi_write_transaction)
+        `uvm_field_int(addr, UVM_ALL_ON)
+        `uvm_field_int(data, UVM_ALL_ON)
+  `uvm_object_utils_end
 
-  function new(string name = "ahb_seq_item");
+  function new(string name = "axi_write_transaction");
     super.new(name);
   endfunction
 endclass
