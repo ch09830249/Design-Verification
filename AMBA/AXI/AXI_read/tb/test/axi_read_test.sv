5fa5f69bf56ac3f90cf914f791e64199c79335e2
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-30 12:25:00 +0800

    Update axi_read_test.sv

--- AMBA/AXI/AXI_read/tb/test/axi_read_test.sv
+++ AMBA/AXI/AXI_read/tb/test/axi_read_test.sv
@@ -1,20 +1,23 @@
-class ahb_test extends uvm_test;
-  `uvm_component_utils(ahb_test)
+class axi_read_test extends uvm_test;
 
-  ahb_env env;
+  `uvm_component_utils(axi_read_test)
 
-  function new(string name, uvm_component parent);
+  axi_read_env env;
+
+  function new(string name = "axi_read_test", uvm_component parent = null);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    env = ahb_env::type_id::create("env", this);
+    super.build_phase(phase);
+    env = axi_read_env::type_id::create("env", this);
   endfunction
 
   task run_phase(uvm_phase phase);
+    axi_read_sequence seq;
     phase.raise_objection(this);
-    apb_write_sequence seq = apb_write_sequence::type_id::create("seq");
-    seq.start(env.agent.driver.seq_item_port);
+    seq = axi_read_sequence::type_id::create("seq");
+    seq.start(env.read_agent.sequencer);
     phase.drop_objection(this);
   endtask
 endclass
