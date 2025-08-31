93025f317c485163e3f04c3e3f357c5f24ea4a53
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-08-31 18:50:53 +0800

    Update tlul_read_agent.sv

--- TLUL/TLUL_get/tb/agent/tlul_read_agent.sv
+++ TLUL/TLUL_get/tb/agent/tlul_read_agent.sv
@@ -1,13 +1,23 @@
-class ahb_agent extends uvm_agent;
-  `uvm_component_utils(ahb_agent)
+class tlul_get_agent extends uvm_agent;
+  `uvm_component_utils(tlul_get_agent)
 
-  ahb_driver driver;
+  tlul_get_sequencer sequencer;
+  tlul_get_driver    driver;
+  tlul_get_monitor   monitor;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
-  function void build_phase(uvm_phase phase);
-    driver = ahb_driver::type_id::create("driver", this);
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    sequencer = tlul_get_sequencer::type_id::create("sequencer", this);
+    driver    = tlul_get_driver   ::type_id::create("driver", this);
+    monitor   = tlul_get_monitor  ::type_id::create("monitor", this);
+  endfunction
+
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
   endfunction
 endclass
