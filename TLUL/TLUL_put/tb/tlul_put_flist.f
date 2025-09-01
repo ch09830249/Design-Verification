387f192a7cf2065446649b2c5c831078d5a210e7
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-01 09:59:34 +0800

    Update and rename tlul_get_flist.f to tlul_put_flist.f

---
+++ TLUL/TLUL_put/tb/tlul_put_flist.f
@@ -0,0 +1,16 @@
+# === RTL and Interface
+./rtl/tlul_put_slave.sv
+
+# === UVM TB Components
+./tb/interface/tlul_if.sv
+./tb/sequence/tlul_transaction.sv
+./tb/sequence/tlul_put_sequence.sv
+./tb/sequence/tlul_put_sequencer.sv
+./tb/agent/tlul_put_monitor.sv
+./tb/agent/tlul_put_driver.sv
+./tb/agent/tlul_put_agent.sv
+./tb/env/tlul_put_env.sv
+./tb/test/tlul_put_test.sv
+
+# === Top-level
+./tb/tlul_put_top.sv
