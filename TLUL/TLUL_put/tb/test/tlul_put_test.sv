697d7af1b05e2272568ee2351fb4bb874b8cb918
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-01 09:59:08 +0800

    Update and rename tlul_get_test.sv to tlul_put_test.sv

--- TLUL/TLUL_put/tb/test/tlul_put_test.sv
+++ TLUL/TLUL_put/tb/test/tlul_put_test.sv
@@ -1,8 +1,8 @@
-class tlul_get_test extends uvm_test;
-  `uvm_component_utils(tlul_get_test)
+class tlul_put_test extends uvm_test;
+  `uvm_component_utils(tlul_put_test)
 
-  tlul_get_env env;
-  tlul_get_sequence seq;
+  tlul_put_env env;
+  tlul_put_sequence seq;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -10,12 +10,12 @@ class tlul_get_test extends uvm_test;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    env = tlul_get_env::type_id::create("env", this);
+    env = tlul_put_env::type_id::create("env", this);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
     phase.raise_objection(this);
-    seq = tlul_get_sequence::type_id::create("seq");
+    seq = tlul_put_sequence::type_id::create("seq");
     seq.start(env.agent.sequencer);
     phase.drop_objection(this);
   endtask
