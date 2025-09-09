caeb9959aad2de6c0a8e8dd8b84dc2b59869e6d5
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 09:58:37 +0800

    Update ahb_write_test.sv

--- AMBA/AHB/AHB_write/tb/test/ahb_write_test.sv
+++ AMBA/AHB/AHB_write/tb/test/ahb_write_test.sv
@@ -1,20 +1,22 @@
-class ahb_test extends uvm_test;
-  `uvm_component_utils(ahb_test)
+class ahb_write_test extends uvm_test;
+  `uvm_component_utils(ahb_write_test)
 
-  ahb_env env;
+  ahb_write_env env;
+  ahb_write_sequence seq;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
-  function void build_phase(uvm_phase phase);
-    env = ahb_env::type_id::create("env", this);
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = ahb_write_env::type_id::create("env", this);
   endfunction
 
-  task run_phase(uvm_phase phase);
+  virtual task run_phase(uvm_phase phase);
     phase.raise_objection(this);
-    apb_write_sequence seq = apb_write_sequence::type_id::create("seq");
-    seq.start(env.agent.driver.seq_item_port);
+    seq = ahb_write_sequence::type_id::create("seq");
+    seq.start(env.agent.sequencer);
     phase.drop_objection(this);
   endtask
 endclass
