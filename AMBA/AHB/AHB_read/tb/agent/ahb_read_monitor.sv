99f6a5b5eb6bce6957b9318fcf1d7a4a70024637
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 16:53:59 +0800

    Update ahb_read_monitor.sv

--- AMBA/AHB/AHB_read/tb/agent/ahb_read_monitor.sv
+++ AMBA/AHB/AHB_read/tb/agent/ahb_read_monitor.sv
@@ -1,37 +1,32 @@
-class ahb_driver extends uvm_driver #(ahb_transaction);
-  `uvm_component_utils(ahb_driver)
+class ahb_read_monitor extends uvm_component;
+  `uvm_component_utils(ahb_read_monitor)
 
   virtual ahb_if vif;
+  uvm_analysis_port #(ahb_transaction) mon_ap;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
+    mon_ap = new("mon_ap", this);
   endfunction
 
   function void build_phase(uvm_phase phase);
     if (!uvm_config_db#(virtual ahb_if)::get(this, "", "vif", vif))
-      `uvm_fatal("NOVIF", "No virtual interface specified for ahb_driver")
+      `uvm_fatal("NOVIF", "Virtual interface not found")
   endfunction
 
   task run_phase(uvm_phase phase);
-    ahb_transaction tx;
+  ahb_transaction tr;
     forever begin
-      seq_item_port.get_next_item(tx);
-
-      // Setup phase
-      vif.HSEL   <= 1;
-      vif.HWRITE <= 1;
-      vif.HADDR  <= tx.addr;
-      vif.HWDATA <= tx.data;
-      vif.HTRANS <= 2'b10; // NONSEQ
-
-      // Wait for HREADY
-      do @(posedge vif.HCLK); while (!vif.HREADYOUT);
-
-      // Done
-      vif.HSEL   <= 0;
-      vif.HTRANS <= 2'b00;
-
-      seq_item_port.item_done();
+      @(posedge vif.HCLK);
+      if (vif.HTRANS == 2'b10 && !vif.HWRITE && vif.HREADY) begin
+        tr = ahb_transaction::type_id::create("tr");
+        tr.addr = vif.HADDR;
+        tr.size = vif.HSIZE;
+        tr.data = vif.HRDATA;
+        `uvm_info("ahb_read_monitor", "Monitor read data", UVM_MEDIUM)
+        tr.print();
+        mon_ap.write(tr);
+      end
     end
   endtask
 endclass
