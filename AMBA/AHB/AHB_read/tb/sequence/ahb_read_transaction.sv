bbea1d166c7b0fe2ede503a4d417ee805d70c510
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 16:55:17 +0800

    Update ahb_read_transaction.sv

--- AMBA/AHB/AHB_read/tb/sequence/ahb_read_transaction.sv
+++ AMBA/AHB/AHB_read/tb/sequence/ahb_read_transaction.sv
@@ -1,10 +1,15 @@
-class ahb_seq_item extends uvm_sequence_item;
+class ahb_transaction extends uvm_sequence_item;
   rand bit [31:0] addr;
   rand bit [31:0] data;
+  rand bit [2:0] size;
 
-  `uvm_object_utils(ahb_seq_item)
+  `uvm_object_utils_begin(ahb_transaction)
+        `uvm_field_int(addr, UVM_ALL_ON)
+        `uvm_field_int(data, UVM_ALL_ON)
+        `uvm_field_int(size, UVM_ALL_ON)
+  `uvm_object_utils_end
 
-  function new(string name = "ahb_seq_item");
+  function new(string name = "ahb_transaction");
     super.new(name);
   endfunction
 endclass
