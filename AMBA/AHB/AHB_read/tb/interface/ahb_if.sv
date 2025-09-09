b970b80a4827fe713cad665eac42e3e84e54384e
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 16:54:45 +0800

    Update and rename ahb_read_if.sv to ahb_if.sv

---
+++ AMBA/AHB/AHB_read/tb/interface/ahb_if.sv
@@ -0,0 +1,16 @@
+interface ahb_if(input logic HCLK, input logic HRESETn);
+  logic [31:0] HADDR;
+  logic [1:0]  HTRANS;
+  logic        HWRITE;
+  logic [2:0]  HSIZE;
+  logic [31:0] HWDATA;
+  logic [31:0] HRDATA;
+  logic        HREADY;
+  logic        HRESP;
+
+  clocking cb @(posedge HCLK);
+    default input #1 output #1;
+    output HADDR, HTRANS, HWRITE, HSIZE, HWDATA;
+    input  HRDATA, HREADY, HRESP;
+  endclocking
+endinterface
