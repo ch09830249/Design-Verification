ee8788e7d68a971e2f1df3064f0798944e3f3f3b
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 09:55:41 +0800

    Update ahb_write_env.sv

--- AMBA/AHB/AHB_write/tb/env/ahb_write_env.sv
+++ AMBA/AHB/AHB_write/tb/env/ahb_write_env.sv
@@ -1,13 +1,14 @@
-class ahb_env extends uvm_env;
-  `uvm_component_utils(ahb_env)
+class ahb_write_env extends uvm_env;
+  `uvm_component_utils(ahb_write_env)
 
-  ahb_agent agent;
+  ahb_write_agent agent;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    agent = ahb_agent::type_id::create("agent", this);
+    super.build_phase(phase);
+    agent = ahb_write_agent::type_id::create("agt", this);
   endfunction
 endclass
