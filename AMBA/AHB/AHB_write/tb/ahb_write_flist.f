8587781cb9df575d2a1b848f723ccdf352136d63
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 09:58:55 +0800

    Update ahb_write_flist.f

--- AMBA/AHB/AHB_write/tb/ahb_write_flist.f
+++ AMBA/AHB/AHB_write/tb/ahb_write_flist.f
@@ -1,42 +1,15 @@
-module top;
+# === RTL and Interface
+./rtl/ahb_write_slave.sv
 
-  logic HCLK;
-  logic HRESETn;
+# === UVM TB Components
+./tb/interface/ahb_if.sv
+./tb/sequence/ahb_transaction.sv
+./tb/sequence/ahb_write_sequence.sv
+./tb/agent/ahb_write_monitor.sv
+./tb/agent/ahb_write_driver.sv
+./tb/agent/ahb_write_agent.sv
+./tb/env/ahb_write_env.sv
+./tb/test/ahb_write_test.sv
 
-  // Instantiate interface
-  ahb_if ahb_if_inst(.HCLK(HCLK), .HRESETn(HRESETn));
-
-  // Clock generation
-  initial begin
-    HCLK = 0;
-    forever #5 HCLK = ~HCLK;
-  end
-
-  // Reset generation
-  initial begin
-    HRESETn = 0;
-    #20 HRESETn = 1;
-  end
-
-  // DUT instance
-  ahb_write_slavel dut (
-    .HCLK      (HCLK),
-    .HRESETn   (HRESETn),
-    .HSEL      (ahb_if_inst.HSEL),
-    .HWRITE    (ahb_if_inst.HWRITE),
-    .HTRANS    (ahb_if_inst.HTRANS),
-    .HADDR     (ahb_if_inst.HADDR),
-    .HWDATA    (ahb_if_inst.HWDATA),
-    .HREADYOUT (ahb_if_inst.HREADYOUT)
-  );
-
-  // Connect VIP to interface
-  initial begin
-    uvm_config_db#(virtual ahb_if)::set(null, "*", "vif", ahb_if_inst);
-  end
-
-  initial begin
-    run_test("ahb_test");
-  end
-
-endmodule
+# === Top-level
+./tb/ahb_write_top.sv
