bbb18d256a7bfda8436fca83ecb5491414005ed4
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 16:55:35 +0800

    Update ahb_read_test.sv

--- AMBA/AHB/AHB_read/tb/test/ahb_read_test.sv
+++ AMBA/AHB/AHB_read/tb/test/ahb_read_test.sv
@@ -1,20 +1,23 @@
-class ahb_test extends uvm_test;
-  `uvm_component_utils(ahb_test)
+class ahb_read_test extends uvm_test;
+  `uvm_component_utils(ahb_read_test)
 
-  ahb_env env;
+  ahb_read_env env;
+  ahb_read_sequence seq;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    env = ahb_env::type_id::create("env", this);
+    env = ahb_read_env::type_id::create("env", this);
   endfunction
 
   task run_phase(uvm_phase phase);
     phase.raise_objection(this);
-    apb_write_sequence seq = apb_write_sequence::type_id::create("seq");
-    seq.start(env.agent.driver.seq_item_port);
+
+    seq = ahb_read_sequence::type_id::create("seq");
+    seq.start(env.agent.seqr);
+
     phase.drop_objection(this);
   endtask
 endclass
