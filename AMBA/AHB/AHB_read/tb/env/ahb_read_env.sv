35eb2eef7caeea187f912b6a592a002fbe142bfb
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-09-09 16:54:20 +0800

    Update ahb_read_env.sv

--- AMBA/AHB/AHB_read/tb/env/ahb_read_env.sv
+++ AMBA/AHB/AHB_read/tb/env/ahb_read_env.sv
@@ -1,13 +1,13 @@
-class ahb_env extends uvm_env;
-  `uvm_component_utils(ahb_env)
+class ahb_read_env extends uvm_env;
+  `uvm_component_utils(ahb_read_env)
 
-  ahb_agent agent;
+  ahb_read_agent agent;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
   function void build_phase(uvm_phase phase);
-    agent = ahb_agent::type_id::create("agent", this);
+    agent = ahb_read_agent::type_id::create("agent", this);
   endfunction
 endclass
