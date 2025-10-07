45503a2bdd16c7f79fdea5024c833d98a0b6a2da
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-10-07 21:02:21 +0800

    Update my_vsqr.sv

--- UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example16_virtual_sequence/my_vsqr.sv
+++ UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example16_virtual_sequence/my_vsqr.sv
@@ -1,8 +1,10 @@
-class my_sequencer extends uvm_sequencer #(my_transaction);
+class my_vsqr extends uvm_sequencer #(my_transaction);
+    my_sequencer p_sqr0;
+    my_sequencer p_sqr1;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
     endfunction
 
-    `uvm_component_utils(my_sequencer)
+    `uvm_component_utils(my_vsqr)
 endclass
