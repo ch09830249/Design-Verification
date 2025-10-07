c364c0116616b62ee6cf7ca03733292a060a398f
ch09830249 <ch09830249@yahoo.com.tw>
2025-10-07 20:23:05 +0800

    Add Example15/16

---
+++ UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example15_multiple_sequence_with_event/my_env.sv
@@ -0,0 +1,45 @@
+`include "my_agent.sv"
+`include "my_model.sv"
+`include "my_scoreboard.sv"
+class my_env extends uvm_env;
+
+    my_agent i_agt;
+    my_agent o_agt;
+    my_model mdl;
+    my_scoreboard scb;
+    uvm_tlm_analysis_fifo #(my_transaction) agt_mdl_fifo;
+    uvm_tlm_analysis_fifo #(my_transaction) mdl_scb_fifo;
+    uvm_tlm_analysis_fifo #(my_transaction) agt_scb_fifo;
+
+    function new(string name = "my_env", uvm_component parent);
+        super.new(name, parent);
+        `uvm_info("my_env", "my_envs is new", UVM_LOW);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        `uvm_info("my_env", "my_env build phase!!", UVM_LOW);
+        uvm_config_db#(uvm_active_passive_enum)::set(this, "i_agt", "is_active", UVM_ACTIVE);
+        uvm_config_db#(uvm_active_passive_enum)::set(this, "o_agt", "is_active", UVM_PASSIVE);
+        i_agt = my_agent::type_id::create("i_agt", this);
+        o_agt = my_agent::type_id::create("o_agt", this);
+        mdl   = my_model::type_id::create("mdl", this);
+        scb   = my_scoreboard::type_id::create("scb", this);
+        agt_mdl_fifo = new("agt_mdl_fifo", this);
+        mdl_scb_fifo = new("mdl_scb_fifo", this);
+        agt_scb_fifo = new("agt_scb_fifo", this);
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        i_agt.ap.connect(agt_mdl_fifo.analysis_export);
+        mdl.port.connect(agt_mdl_fifo.blocking_get_export);
+        mdl.ap.connect(mdl_scb_fifo.analysis_export);
+        scb.exp_port.connect(mdl_scb_fifo.blocking_get_export);
+        o_agt.ap.connect(agt_scb_fifo.analysis_export);
+        scb.act_port.connect(agt_scb_fifo.blocking_get_export);
+    endfunction
+
+    `uvm_component_utils(my_env)
+
+endclass
---
+++ UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example15_multiple_sequence_with_event/my_sequencer.sv
@@ -0,0 +1,9 @@
+`include "my_sequence.sv"
+class my_sequencer extends uvm_sequencer #(my_transaction);
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    `uvm_component_utils(my_sequencer)
+endclass
