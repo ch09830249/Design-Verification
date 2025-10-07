159d2c0f0de41ac51654fc1b6c3c6c1221656033
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-10-07 20:30:41 +0800

    Update dut.sv

--- UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example15_multiple_sequence_with_event/dut.sv
+++ UVM/UVM_實戰_張強/Ch2_UVM_Testbench/Example15_multiple_sequence_with_event/dut.sv
@@ -1,22 +1,40 @@
-module dut(clk, rst_n, rxd, rx_dv, txd, tx_en);
+/*
+    DUT 的增加了一組資料口，這組新的資料口與原先的資料口功能完全一樣
+*/
+module dut(clk, rst_n, 
+            rxd0, rx_dv0, txd0, tx_en0, 
+            rxd1, rx_dv1, txd1, tx_en1);
     input clk;
     input rst_n;
-    input[7:0] rxd;
-    input rx_dv;
-    output [7:0] txd;
-    output tx_en;
+    
+    input[7:0] rxd0;
+    input rx_dv0;
+    output [7:0] txd0;
+    output tx_en0;
 
-    reg[7:0] txd;
-    reg tx_en;
+    input[7:0] rxd1;
+    input rx_dv1;
+    output [7:0] txd1;
+    output tx_en1;
+
+    reg[7:0] txd0;
+    reg tx_en0;
+
+    reg[7:0] txd1;
+    reg tx_en1;
 
     always @(posedge clk) begin
         if(!rst_n) begin
-            txd <= 8'b0;
-            tx_en <= 1'b0;
+            txd0 <= 8'b0;
+            tx_en0 <= 1'b0;
+            txd1 <= 8'b0;
+            tx_en1 <= 1'b0;
         end
         else begin
-            txd <= rxd;
-            tx_en <= rx_dv;
+            txd0 <= rxd0;
+            tx_en0 <= rx_dv0;
+            txd1 <= rxd1;
+            tx_en1 <= rx_dv1;
         end
     end
 endmodule
