2346abde1a2caecd92645a4f21eecd9d07e5d4ab
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-11-01 16:50:11 +0800

    Update axi_basic_seq.sv

--- AMBA/AXI/AXI_simple_vip/tb/sequence/axi_basic_seq.sv
+++ AMBA/AXI/AXI_simple_vip/tb/sequence/axi_basic_seq.sv
@@ -1,18 +1,33 @@
-class axi_write_sequence extends uvm_sequence #(axi_write_transaction);
-  `uvm_object_utils(axi_write_sequence)
+class axi_basic_seq extends uvm_sequence #(axi_txn);
+    `uvm_object_utils(axi_basic_seq)
 
-  function new(string name = "axi_write_sequence");
-    super.new(name);
-  endfunction
+    function new(string name = "axi_basic_seq");
+        super.new(name);
+    endfunction
 
-  virtual task body();
-    axi_write_transaction tr;
-    for (int i = 0; i < 3; i++) begin       // 發送 3 筆 write
-      tr = axi_write_transaction::type_id::create("tr");
-      assert(tr.randomize());
-      tr.print();
-      start_item(tr);
-      finish_item(tr);
-    end
-  endtask
+    task body();
+        axi_txn tr;
+
+        repeat (5) begin
+            tr = axi_txn::type_id::create("tr");
+            tr.id         = $urandom_range(0, 15);
+            tr.addr       = $urandom();
+            tr.burst_len  = 4;
+            tr.burst_type = 2'b01;
+            tr.burst_size = 3'b010;
+            tr.burst_size = 3'b000;
+            tr.lock       = 0;                          // Normal Access
+            tr.qos        = $urandom_range(0, 15);
+            tr.wstrb      = $urandom_range(0, 15);
+            tr.is_write   = $urandom_range(0, 1);
+
+            for (int i = 0; i <= tr.burst_len; i++)
+                tr.data.push_back($urandom());
+
+            `uvm_info(get_type_name(), "Sending txn", UVM_MEDIUM)
+
+            start_item(tr);
+            finish_item(tr);
+        end
+    endtask
 endclass
