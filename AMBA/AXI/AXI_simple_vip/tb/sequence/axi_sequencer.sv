a7a8c533349aa1fd3a4db7c19ff0a40feef0b057
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-11-01 16:50:32 +0800

    Update axi_sequencer.sv

--- AMBA/AXI/AXI_simple_vip/tb/sequence/axi_sequencer.sv
+++ AMBA/AXI/AXI_simple_vip/tb/sequence/axi_sequencer.sv
@@ -1,18 +1,7 @@
-class axi_write_sequence extends uvm_sequence #(axi_write_transaction);
-  `uvm_object_utils(axi_write_sequence)
+class axi_sequencer extends uvm_sequencer #(axi_txn);
+    `uvm_component_utils(axi_sequencer)
 
-  function new(string name = "axi_write_sequence");
-    super.new(name);
-  endfunction
-
-  virtual task body();
-    axi_write_transaction tr;
-    for (int i = 0; i < 3; i++) begin       // 發送 3 筆 write
-      tr = axi_write_transaction::type_id::create("tr");
-      assert(tr.randomize());
-      tr.print();
-      start_item(tr);
-      finish_item(tr);
-    end
-  endtask
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
 endclass
