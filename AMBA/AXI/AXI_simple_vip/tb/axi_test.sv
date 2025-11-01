6de13f4c5ea796a4c839304d398bc5932eb2dc5e
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-11-01 16:52:32 +0800

    Update axi_test.sv

--- AMBA/AXI/AXI_simple_vip/tb/axi_test.sv
+++ AMBA/AXI/AXI_simple_vip/tb/axi_test.sv
@@ -1,21 +1,25 @@
-class axi_write_test extends uvm_test;
-  `uvm_component_utils(axi_write_test)
+// 定義基本測試與 sequence
+class axi_test extends uvm_test;
+    `uvm_component_utils(axi_test)
 
-  axi_write_env env;
-  axi_write_sequence seq;
+    axi_env env;
 
-  function new(string name = "axi_write_test", uvm_component parent = null); 
-    super.new(name, parent);
-  endfunction
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
 
-  function void build_phase(uvm_phase phase);
-    env = axi_write_env::type_id::create("env", this);
-  endfunction
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        env = axi_env::type_id::create("env", this);
+    endfunction
 
-  task run_phase(uvm_phase phase);
-    phase.raise_objection(this);
-    seq = axi_write_sequence::type_id::create("seq");
-    seq.start(env.write_agent.sequencer);
-    phase.drop_objection(this);
-  endtask
+    task run_phase(uvm_phase phase);
+        axi_basic_seq seq;
+        phase.raise_objection(this);
+
+        seq = axi_basic_seq::type_id::create("seq");
+        seq.start(env.master_agent.sequencer);
+
+        phase.drop_objection(this);
+    endtask
 endclass
