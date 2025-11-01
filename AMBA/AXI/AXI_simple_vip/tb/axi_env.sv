9eb749047ff5e75a31fbae56204280551dbe9916
Kenny <59005836+ch09830249@users.noreply.github.com>
2025-11-01 16:51:17 +0800

    Update axi_env.sv

--- AMBA/AXI/AXI_simple_vip/tb/axi_env.sv
+++ AMBA/AXI/AXI_simple_vip/tb/axi_env.sv
@@ -1,15 +1,24 @@
-class axi_write_env extends uvm_env;
+// 用於統合 master/slave agent
+class axi_env extends uvm_env;
+    `uvm_component_utils(axi_env)
 
-  `uvm_component_utils(axi_write_env)
+    axi_agent master_agent;
+    axi_agent slave_agent;
 
-  axi_write_agent write_agent;
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
 
-  function new(string name = "axi_write_env", uvm_component parent = null);  
-    super.new(name, parent);
-  endfunction
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
 
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    write_agent = axi_write_agent::type_id::create("write_agent", this);
-  endfunction
+        master_agent = axi_agent::type_id::create("master_agent", this);
+        slave_agent  = axi_agent::type_id::create("slave_agent", this);
+
+        uvm_config_db#(bit)::set(this, "master_agent", "is_master", 1);
+        uvm_config_db#(bit)::set(this, "slave_agent",  "is_master", 0);
+
+        uvm_config_db#(uvm_active_passive_enum)::set(this, "master_agent", "is_active", UVM_ACTIVE);
+        uvm_config_db#(uvm_active_passive_enum)::set(this, "slave_agent",  "is_active", UVM_PASSIVE);
+    endfunction
 endclass
