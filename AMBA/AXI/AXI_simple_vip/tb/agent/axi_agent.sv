e43d72b04baf523e25d85c60d5b936780d037cf3
Kenny <59005836+ch09830249@users.noreply.github.com>
2026-03-30 00:44:52 +0800

    Update axi_agent.sv

--- AMBA/AXI/AXI_simple_vip/tb/agent/axi_agent.sv
+++ AMBA/AXI/AXI_simple_vip/tb/agent/axi_agent.sv
@@ -1,14 +1,10 @@
-// 整合 driver、sequencer、monitor，支援 active/passive 兩種模式
 class axi_agent extends uvm_agent;
     `uvm_component_utils(axi_agent)
 
-    axi_sequencer     sequencer;
-    axi_driver        driver;
-    axi_monitor       monitor;
-
-    virtual axi_if    vif;
-    bit               is_master;
-    uvm_active_passive_enum is_active;
+    axi_sequencer               sequencer;
+    axi_driver                  driver;
+    axi_monitor                 monitor;
+    uvm_active_passive_enum     is_active;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -17,14 +13,8 @@ class axi_agent extends uvm_agent;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        if (!uvm_config_db#(virtual axi_if)::get(this, "", "vif", vif))
-            `uvm_fatal("AXI_AGENT", "Virtual interface not found")
-
-        if (!uvm_config_db#(bit)::get(this, "", "is_master", is_master))
-            is_master = 1;
-
         if (!uvm_config_db#(uvm_active_passive_enum)::get(this, "", "is_active", is_active))
-            is_active = UVM_ACTIVE;
+            `uvm_fatal("AXI_AGT", "Get is_active failed!")
 
         if (is_active == UVM_ACTIVE) begin
             sequencer = axi_sequencer::type_id::create("sequencer", this);
@@ -37,7 +27,7 @@ class axi_agent extends uvm_agent;
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
         if (is_active == UVM_ACTIVE) begin
-            driver.seq_item_port.connect(sequencer.seq_item_export);
+            driver.seq_item_port.connect(sequencer.seq_item_export);            // driver 和 sequencer 連接
         end
     endfunction
 endclass
