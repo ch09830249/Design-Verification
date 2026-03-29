735f0daafd1c7008e6fa085414b2ef90fa21ff2c
Kenny <59005836+ch09830249@users.noreply.github.com>
2026-03-30 00:48:02 +0800

    Update axi_env.sv

--- AMBA/AXI/AXI_simple_vip/tb/axi_env.sv
+++ AMBA/AXI/AXI_simple_vip/tb/axi_env.sv
@@ -15,8 +15,8 @@ class axi_env extends uvm_env;
         master_agent = axi_agent::type_id::create("master_agent", this);
         slave_agent  = axi_agent::type_id::create("slave_agent", this);
 
-        uvm_config_db#(bit)::set(this, "master_agent", "is_master", 1);
-        uvm_config_db#(bit)::set(this, "slave_agent",  "is_master", 0);
+        uvm_config_db#(bit)::set(this, "master_agent*", "is_master", 1);    // 自己 + 所有子層
+        uvm_config_db#(bit)::set(this, "slave_agent*",  "is_master", 0);    
 
         uvm_config_db#(uvm_active_passive_enum)::set(this, "master_agent", "is_active", UVM_ACTIVE);
         uvm_config_db#(uvm_active_passive_enum)::set(this, "slave_agent",  "is_active", UVM_PASSIVE);
