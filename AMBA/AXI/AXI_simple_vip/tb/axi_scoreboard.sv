c70f5fa1b511174fb430aef8afb3d5aec0c63115
Kenny <59005836+ch09830249@users.noreply.github.com>
2026-03-30 02:04:03 +0800

    Update axi_scoreboard.sv

--- AMBA/AXI/AXI_simple_vip/tb/axi_scoreboard.sv
+++ AMBA/AXI/AXI_simple_vip/tb/axi_scoreboard.sv
@@ -1,11 +1,11 @@
 class axi_scoreboard extends uvm_component;
   `uvm_component_utils(axi_scoreboard)
-  // uvm_analysis_export#(axi_txn) mon_export;
+  uvm_tlm_analysis_fifo #(axi_txn) fifo;
 
 
   function new(string name = "axi_scoreboard", uvm_component parent = null);
     super.new(name, parent);
-    // mon_export = new("mon_export", this);
+    fifo = new("fifo", this);
   endfunction
 
 
@@ -13,13 +13,13 @@ class axi_scoreboard extends uvm_component;
     super.build_phase(phase);
   endfunction
 
-
   task run_phase(uvm_phase phase);
     axi_txn tr;
     forever begin
-      // mon_export.analysis_if.read(tr);
-      // do simple check: just print events for now
-      `uvm_info(get_type_name(), $sformatf("Scoreboard observed: %s", tr.convert2string()), UVM_LOW)
+      fifo.get(tr);
+      `uvm_info(get_type_name(),
+        $sformatf("Scoreboard got txn:\n%s", tr.sprint()),
+        UVM_LOW)
     end
   endtask
 
