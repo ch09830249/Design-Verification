560e4a51886626dc7e256fd60d1e9195c8072834
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-16 13:17:27 +0800

    Update sim_top.sv

--- Protocol/AMBA/APB/APB_VIP/top/sim_top.sv
+++ Protocol/AMBA/APB/APB_VIP/top/sim_top.sv
@@ -17,8 +17,8 @@ module sim_top;
     logic           clk, rst_n;
     apb_interface   vif();
 
-    assign vif.PCLK     = clk;
-    assign vif.PRESETn  = rst_n;
+    assign vif.PCLK    = clk;
+    assign vif.PRESETn = rst_n;
 
     // apb_slave_bfm slv (
     //     .PCLK   (clk),
@@ -26,35 +26,23 @@ module sim_top;
     //     .vif    (vif)
     // );
 
-    always  #5  clk = ~clk;
+    always #5 clk = ~clk;
+
+    initial begin
+        clk   = 0;
+        rst_n = 0;
+        uvm_config_db #(virtual apb_interface) :: set (null, "*", "vif", vif);
+        #10;
+        rst_n = 1;
+    end
+
+    initial begin
+        $shm_open("wave.shm");
+        $shm_probe("AS");
+    end
 
     initial begin
         run_test();
-    end
-
-    initial begin
-
-        uvm_config_db #(virtual apb_interface) :: set (null, "*", "vif", vif);
-
-        clk     = 0;
-        rst_n   = 0;
-
-        #10;
-        rst_n   = 1;
-    end
-
-    initial begin
-        // original
-        // $fsdbDumpfile("wave.fsdb");
-        // $fsdbDumpvars;
-
-        // 改成 (Xcelium 原生 shm 格式)
-        $shm_open("wave.shm");
-        $shm_probe("AS");
-
-        // 或改成標準 VCD
-        // $dumpfile("wave.vcd");
-        // $dumpvars(0, sim_top);
     end
 
 endmodule
