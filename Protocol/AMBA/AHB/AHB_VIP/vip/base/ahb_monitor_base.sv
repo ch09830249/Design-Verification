9882a813d26b1dfde66c102d9473b6eab5190108
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:20:34 +0800

    Update ahb_monitor_base.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/base/ahb_monitor_base.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/base/ahb_monitor_base.sv
@@ -1,26 +1,23 @@
-`ifndef APB_MONITOR_BASE_SV
-`define APB_MONITOR_BASE_SV
+`ifndef AHB_MONITOR_BASE_SV
+`define AHB_MONITOR_BASE_SV
 
 class ahb_monitor_base extends uvm_monitor;
-    `uvm_component_utils(apb_monitor_base)
+    `uvm_component_utils(ahb_monitor_base)
 
-    virtual apb_interface               vif;
-
-    apb_seq_item                        txn;
-
+    virtual ahb_interface               vif;
+    ahb_seq_item                        txn;
     uvm_active_passive_enum             agt_mode;
+    uvm_analysis_port #(ahb_seq_item)   port;
 
-    uvm_analysis_port #(apb_seq_item)   port;
-
-    function new ( string name = "apb_monitor_base", uvm_component parent );
+    function new ( string name = "ahb_monitor_base", uvm_component parent );
         super.new(name, parent);
-        port = new("port", this);
     endfunction
 
     function void build_phase ( uvm_phase phase );
         super.build_phase(phase);
+        port = new("port", this);
 
-        if ( !uvm_config_db #(virtual apb_interface) :: get (this, "", "vif", vif) )
+        if ( !uvm_config_db #(virtual ahb_interface) :: get (this, "", "vif", vif) )
             `uvm_fatal("NOVIF", $sformatf("No interface set for %s.vif", get_full_name() ))
 
         if ( !uvm_config_db #(uvm_active_passive_enum) :: get (this, "", "agt_mode", agt_mode) )
