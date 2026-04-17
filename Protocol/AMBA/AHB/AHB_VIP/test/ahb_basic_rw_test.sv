80dd8076cf303bf00745cba8799c0ee76cd1ec6d
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:19:04 +0800

    Update ahb_basic_rw_test.sv

--- Protocol/AMBA/AHB/AHB_VIP/test/ahb_basic_rw_test.sv
+++ Protocol/AMBA/AHB/AHB_VIP/test/ahb_basic_rw_test.sv
@@ -1,31 +1,31 @@
-`ifndef APB_BASIC_RW_TEST_SV
-`define APB_BASIC_RW_TEST_SV
+`ifndef AHB_BASIC_RW_TEST_SV
+`define AHB_BASIC_RW_TEST_SV
 
 class ahb_basic_rw_test extends uvm_test;
-    `uvm_component_utils(apb_basic_rw_test)
+    `uvm_component_utils(ahb_basic_rw_test)
 
-    apb_env                     env;
-    apb_master_seq              mst_seq;
-    apb_slave_seq               slv_seq;
+    ahb_env                     env;
+    ahb_master_seq              mst_seq;
+    ahb_slave_seq               slv_seq;
 
-    function new ( string name = "apb_basic_rw_test", uvm_component parent );
+    function new ( string name = "ahb_basic_rw_test", uvm_component parent );
         super.new(name, parent);
     endfunction
 
     function void build_phase ( uvm_phase phase );
         super.build_phase(phase);
-        env = apb_env :: type_id :: create ("env", this);
+        env = ahb_env :: type_id :: create ("env", this);
     endfunction
 
     virtual task run_phase ( uvm_phase phase );
         phase.raise_objection(this);
 
-        mst_seq = apb_master_seq :: type_id :: create("mst_seq");
-        slv_seq = apb_slave_seq :: type_id :: create("slv_seq");
+        mst_seq = ahb_master_seq :: type_id :: create("mst_seq");
+        slv_seq = ahb_slave_seq :: type_id :: create("slv_seq");
         fork
             begin
                 mst_seq.start(env.agt_mst.seqr);
-                #50ps;
+                repeat(10) @ (posedge env.vif.HCLK);  // ensure the last transfer done
             end
             begin
                 // will be waiting for req forever
