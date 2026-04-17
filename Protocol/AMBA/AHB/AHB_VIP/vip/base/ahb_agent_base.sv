c64ae29080ca69efb4f99f74450dfa9a2f7cdf90
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:20:05 +0800

    Update ahb_agent_base.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/base/ahb_agent_base.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/base/ahb_agent_base.sv
@@ -1,16 +1,16 @@
-`ifndef APB_AGENT_BASE_SV
-`define APB_AGENT_BASE_SV
+`ifndef AHB_AGENT_BASE_SV
+`define AHB_AGENT_BASE_SV
 
 class ahb_agent_base extends uvm_agent;
-    `uvm_component_utils(apb_agent_base)
+    `uvm_component_utils(ahb_agent_base)
 
-    apb_driver_base                     drv;
-    apb_monitor_base                    mon;
-    uvm_sequencer #(apb_seq_item)       seqr;
+    ahb_driver_base                     drv;
+    ahb_monitor_base                    mon;
+    uvm_sequencer #(ahb_seq_item)       seqr;
 
     uvm_active_passive_enum             agt_mode;
 
-    function new ( string name = "apb_agent_base", uvm_component parent );
+    function new ( string name = "ahb_agent_base", uvm_component parent );
         super.new(name, parent);
     endfunction
 
@@ -22,17 +22,17 @@ class ahb_agent_base extends uvm_agent;
 
         uvm_config_db #(uvm_active_passive_enum) :: set (this, "*", "agt_mode", agt_mode);
 
-        mon = apb_monitor_base :: type_id :: create ("mon", this);
+        mon = ahb_monitor_base :: type_id :: create ("mon", this);
 
         if ( agt_mode == UVM_ACTIVE ) begin
-            drv = apb_driver_base :: type_id :: create ("drv", this);
-            seqr = uvm_sequencer #(apb_seq_item) :: type_id :: create ("seqr", this);
+            drv  = ahb_driver_base :: type_id :: create ("drv", this);
+            seqr = uvm_sequencer #(ahb_seq_item) :: type_id :: create ("seqr", this);
         end
     endfunction
 
     function void connect_phase ( uvm_phase phase );
         super.connect_phase(phase);
-        
+
         if ( agt_mode == UVM_ACTIVE )
             drv.seq_item_port.connect(seqr.seq_item_export);
     endfunction
