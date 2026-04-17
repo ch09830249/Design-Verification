124ac4083c6f02957cc7e8726c5c1d1064a80ea3
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:21:19 +0800

    Update ahb_define.svh

--- Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_define.svh
+++ Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_define.svh
@@ -1,12 +1,28 @@
-`ifndef APB_DEFINE_SVH
-`define APB_DEFINE_SVH
+`ifndef AHB_DEFINE_SVH
+`define AHB_DEFINE_SVH
 
 `define     D_ADDR_WIDTH        64
 `define     D_DATA_WIDTH        32
 `define     D_SLV_COUNT         1
-
 `define     D_MEM_SIZE          256
 
-// typedef enum { MST, SLV }       apb_role_e;
+// HTRANS
+`define     HTRANS_IDLE         2'b00
+`define     HTRANS_BUSY         2'b01
+`define     HTRANS_NONSEQ       2'b10
+`define     HTRANS_SEQ          2'b11
+
+// HBURST
+`define     HBURST_SINGLE       3'b000
+`define     HBURST_INCR         3'b001
+
+// HSIZE
+`define     HSIZE_BYTE          3'b000
+`define     HSIZE_HALFWORD      3'b001
+`define     HSIZE_WORD          3'b010
+
+// HRESP
+`define     HRESP_OKAY          1'b0
+`define     HRESP_ERROR         1'b1
 
 `endif
