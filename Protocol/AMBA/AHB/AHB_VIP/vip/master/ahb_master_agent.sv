00835e1d37e37d4e43bd6ae038a6b11a2b16c4aa
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:23:55 +0800

    Update ahb_master_agent.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/master/ahb_master_agent.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/master/ahb_master_agent.sv
@@ -1,24 +1,24 @@
-`ifndef APB_MASTER_AGENT_SV
-`define APB_MASTER_AGENT_SV
+`ifndef AHB_MASTER_AGENT_SV
+`define AHB_MASTER_AGENT_SV
 
 class ahb_master_agent extends ahb_agent_base;
-    `uvm_component_utils(apb_master_agent)
+    `uvm_component_utils(ahb_master_agent)
 
-    function new ( string name = "apb_master_agent", uvm_component parent );
+    function new ( string name = "ahb_master_agent", uvm_component parent );
         super.new(name, parent);
     endfunction
 
     function void build_phase ( uvm_phase phase );
 
         factory.set_inst_override_by_type (
-            apb_driver_base::get_type(),
-            apb_master_driver::get_type(),
+            ahb_driver_base::get_type(),
+            ahb_master_driver::get_type(),
             "*agt_mst.*"
         );
 
         factory.set_inst_override_by_type (
-            apb_monitor_base::get_type(),
-            apb_master_monitor::get_type(),
+            ahb_monitor_base::get_type(),
+            ahb_master_monitor::get_type(),
             "*agt_mst.*"
         );
 
