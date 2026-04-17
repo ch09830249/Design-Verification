60fe9a8fff8a09325b391f8f6d0c04653aa7661d
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:18:23 +0800

    Update ahb_master_seq.sv

--- Protocol/AMBA/AHB/AHB_VIP/seq/ahb_master_seq.sv
+++ Protocol/AMBA/AHB/AHB_VIP/seq/ahb_master_seq.sv
@@ -1,19 +1,19 @@
-`ifndef APB_MASTER_SEQ_SV
-`define APB_MASTER_SEQ_SV
+`ifndef AHB_MASTER_SEQ_SV
+`define AHB_MASTER_SEQ_SV
 
-class ahb_master_seq extends uvm_sequence #(apb_seq_item);
-    `uvm_object_utils(apb_master_seq)
+class ahb_master_seq extends uvm_sequence #(ahb_seq_item);
+    `uvm_object_utils(ahb_master_seq)
 
-    apb_seq_item        txn;
+    ahb_seq_item        txn;
 
-    function new ( string name = "apb_master_seq" );
+    function new ( string name = "ahb_master_seq" );
         super.new(name);
     endfunction
 
     virtual task body();
-        for ( int i=0; i<10000; i+=1 ) begin
-            txn = apb_seq_item :: type_id :: create ("txn");
-            
+        for ( int i=0; i<10; i+=1 ) begin
+            txn = ahb_seq_item :: type_id :: create ("txn");
+
             if ( !txn.randomize() )
                 `uvm_fatal("RANDERR", "txn randomize fail!")
 
