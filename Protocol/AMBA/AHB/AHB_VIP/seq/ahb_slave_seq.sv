69c42dcf322b49431afd43f2cece84945f74c26e
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:18:39 +0800

    Update ahb_slave_seq.sv

--- Protocol/AMBA/AHB/AHB_VIP/seq/ahb_slave_seq.sv
+++ Protocol/AMBA/AHB/AHB_VIP/seq/ahb_slave_seq.sv
@@ -1,25 +1,24 @@
-`ifndef APB_SLAVE_SEQ_SV
-`define APB_SLAVE_SEQ_SV
+`ifndef AHB_SLAVE_SEQ_SV
+`define AHB_SLAVE_SEQ_SV
 
 class ahb_slave_seq extends uvm_sequence;
-    `uvm_object_utils(apb_slave_seq)
+    `uvm_object_utils(ahb_slave_seq)
 
-    apb_seq_item                            txn;
-    uvm_tlm_analysis_fifo #(apb_seq_item)   fifo;
+    ahb_seq_item                            txn;
+    uvm_tlm_analysis_fifo #(ahb_seq_item)   fifo;
 
-    function new ( string name = "apb_slave_seq" );
+    function new ( string name = "ahb_slave_seq" );
         super.new(name);
     endfunction
 
     virtual task pre_start();
-        if ( !uvm_config_db #(uvm_tlm_analysis_fifo #(apb_seq_item)) :: get (null, get_full_name(), "fifo", fifo) )
+        if ( !uvm_config_db #(uvm_tlm_analysis_fifo #(ahb_seq_item)) :: get (null, get_full_name(), "fifo", fifo) )
             `uvm_fatal("NOFIFO", $sformatf("No fifo set for %s.fifo", get_full_name() ))
     endtask
 
     virtual task body();
         forever begin
             fifo.get(txn);
-            // txn.print();
             start_item(txn);
             finish_item(txn);
         end
