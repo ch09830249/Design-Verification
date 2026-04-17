e3ed8467fa3bb47208f576b95250d6e3d2374af4
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:26:11 +0800

    Update bind_ahb_protocol_sva.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/sva/bind_ahb_protocol_sva.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/sva/bind_ahb_protocol_sva.sv
@@ -2,16 +2,19 @@
 `define BIND_AHB_PROTOCOL_SVA_SV
 
 bind sim_top ahb_protocol_sva bind_ahb_sva (
-    .PCLK(vif.PCLK),
-    .PRESETn(vif.PRESETn),
-    .PADDR(vif.PADDR),
-    .PWRITE(vif.PWRITE),
-    .PSEL(vif.PSEL),
-    .PENABLE(vif.PENABLE),
-    .PWDATA(vif.PWDATA),
-    .PREADY(vif.PREADY),
-    .PRDATA(vif.PRDATA),
-    .PSLVERR(vif.PSLVERR)
+    .HCLK(vif.HCLK),
+    .HRESETn(vif.HRESETn),
+    .HADDR(vif.HADDR),
+    .HWRITE(vif.HWRITE),
+    .HSEL(vif.HSEL),
+    .HTRANS(vif.HTRANS),
+    .HBURST(vif.HBURST),
+    .HSIZE(vif.HSIZE),
+    .HWDATA(vif.HWDATA),
+    .HMASTLOCK(vif.HMASTLOCK),
+    .HRDATA(vif.HRDATA),
+    .HREADY(vif.HREADY),
+    .HRESP(vif.HRESP)
 );
 
 `endif
