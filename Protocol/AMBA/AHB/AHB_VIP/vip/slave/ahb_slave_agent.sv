bc8aaf64936d51a399f3663a80683a53819cfec7
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:24:55 +0800

    Update ahb_slave_agent.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/slave/ahb_slave_agent.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/slave/ahb_slave_agent.sv
@@ -1,24 +1,24 @@
-`ifndef APB_SLAVE_AGENT_SV
-`define APB_SLAVE_AGENT_SV
+`ifndef AHB_SLAVE_AGENT_SV
+`define AHB_SLAVE_AGENT_SV
 
 class ahb_slave_agent extends ahb_agent_base;
-    `uvm_component_utils(apb_slave_agent)
+    `uvm_component_utils(ahb_slave_agent)
 
-    function new ( string name = "apb_slave_agent", uvm_component parent );
+    function new ( string name = "ahb_slave_agent", uvm_component parent );
         super.new(name, parent);
     endfunction
 
     function void build_phase ( uvm_phase phase );
 
         factory.set_inst_override_by_type (
-            apb_driver_base::get_type(),
-            apb_slave_driver::get_type(),
+            ahb_driver_base::get_type(),
+            ahb_slave_driver::get_type(),
             "*agt_slv.*"
         );
 
         factory.set_inst_override_by_type (
-            apb_monitor_base::get_type(),
-            apb_slave_monitor::get_type(),
+            ahb_monitor_base::get_type(),
+            ahb_slave_monitor::get_type(),
             "*agt_slv.*"
         );
 
