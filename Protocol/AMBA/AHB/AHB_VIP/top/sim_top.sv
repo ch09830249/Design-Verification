de68690d932ca05e69c1a74181fb98dd3c724cd1
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-17 08:19:31 +0800

    Update sim_top.sv

--- Protocol/AMBA/AHB/AHB_VIP/top/sim_top.sv
+++ Protocol/AMBA/AHB/AHB_VIP/top/sim_top.sv
@@ -1,60 +1,47 @@
 `timescale 1ps/1ps
 
-`include "apb_define.svh"
-`include "apb_protocol_sva.sv"
-`include "apb_interface.sv"
-`include "bind_apb_protocol_sva.sv"
+`include "ahb_define.svh"
+`include "ahb_protocol_sva.sv"
+`include "ahb_interface.sv"
+`include "bind_ahb_protocol_sva.sv"
 
 import uvm_pkg::*;
 
-`include "apb_package.svh"
-import apb_package::*;
-
-// `include "apb_slave_bfm.sv"
+`include "ahb_package.svh"
+import ahb_package::*;
 
 module sim_top;
 
     logic           clk, rst_n;
-    apb_interface   vif();
+    ahb_interface   vif();
 
-    assign vif.PCLK     = clk;
-    assign vif.PRESETn  = rst_n;
+    assign vif.HCLK    = clk;
+    assign vif.HRESETn = rst_n;
 
-    // apb_slave_bfm slv (
+    // ahb_slave_bfm slv (
     //     .PCLK   (clk),
     //     .PRESETn(rst_n),
     //     .vif    (vif)
     // );
 
-    always  #5  clk = ~clk;
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk   = 0;
+        rst_n = 0;
+        uvm_config_db #(virtual ahb_interface) :: set (null, "*", "vif", vif);
+        #10;
+        rst_n = 1;
+    end
+
+    initial begin
+        $shm_open("wave.shm");
+        $shm_probe("AS");
+    end
 
     initial begin
         run_test();
-    end
-
-    initial begin
-
-        uvm_config_db #(virtual apb_interface) :: set (null, "*", "vif", vif);
-
-        clk     = 0;
-        rst_n   = 0;
-
-        #10;
-        rst_n   = 1;
-    end
-
-    initial begin
-        // original
-        // $fsdbDumpfile("wave.fsdb");
-        // $fsdbDumpvars;
-
-        // 改成 (Xcelium 原生 shm 格式)
-        $shm_open("wave.shm");
-        $shm_probe("AS");
-
-        // 或改成標準 VCD
-        // $dumpfile("wave.vcd");
-        // $dumpvars(0, sim_top);
     end
 
 endmodule
