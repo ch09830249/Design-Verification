69804eafabf157b23c52989f7cf398a3ccdc6d92
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-23 17:53:53 +0800

    Update ahb_seq_item.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_seq_item.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_seq_item.sv
@@ -7,18 +7,18 @@ class ahb_seq_item extends uvm_sequence_item;
 
     rand bit [`D_ADDR_WIDTH-1:0]    HADDR;
     rand bit                        HWRITE;
-    rand bit [`D_SLV_COUNT-1:0]     HSEL;
     rand bit [`D_DATA_WIDTH-1:0]    HWDATA;
     rand bit [2:0]                  HBURST;
     rand bit [2:0]                  HSIZE;
-    rand bit [1:0]                  HTRANS;
-    rand bit [3:0]                  HPROT;
-    rand bit                        HMASTLOCK;
     rand int unsigned               burst_len;
 
     bit [`D_DATA_WIDTH-1:0]         HRDATA;
     bit                             HREADY;
     bit                             HRESP;
+    bit [1:0]                       HTRANS    = `HTRANS_NONSEQ;
+    bit [3:0]                       HPROT     = 4'b0011;    // Data/Privileged access
+    bit                             HMASTLOCK = 0;
+    bit [`D_SLV_COUNT-1:0]          HSEL      = 1;
 
     `uvm_object_utils_begin(ahb_seq_item)
         `uvm_field_int(HADDR,     UVM_ALL_ON)
@@ -36,11 +36,10 @@ class ahb_seq_item extends uvm_sequence_item;
         `uvm_field_int(HRESP,     UVM_ALL_ON)
     `uvm_object_utils_end
 
-    constraint htrans_valid_c    { HTRANS inside {`HTRANS_IDLE, `HTRANS_NONSEQ, `HTRANS_SEQ}; }
     constraint hsize_valid_c     { HSIZE  inside {`HSIZE_BYTE, `HSIZE_HALFWORD, `HSIZE_WORD}; }
     constraint hburst_valid_c    { HBURST inside {`HBURST_SINGLE, `HBURST_INCR}; }
     constraint hsel_onehot_c     { $onehot(HSEL); }
-    constraint addr_overflow_c   { HADDR < `D_MEM_SIZE; }
+    constraint addr_overflow_c   { HADDR + (burst_len * (1 << HSIZE)) <= `D_MEM_SIZE; }
     constraint align_addr_c      { HADDR % (1 << HSIZE) == 0; }
     constraint burst_len_valid_c { burst_len inside {[1:16]}; }
     constraint single_len_c      { ( HBURST == `HBURST_SINGLE ) -> ( burst_len == 1 ); }
