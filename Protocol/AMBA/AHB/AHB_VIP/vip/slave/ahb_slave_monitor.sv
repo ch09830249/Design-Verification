b49c38ee0102e31c331d740831852decd2459c72
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-23 17:57:05 +0800

    Update ahb_slave_monitor.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/slave/ahb_slave_monitor.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/slave/ahb_slave_monitor.sv
@@ -9,22 +9,23 @@ class ahb_slave_monitor extends ahb_monitor_base;
     endfunction
 
     virtual task run_phase ( uvm_phase phase );
+        wait ( !vif.HRESETn );
+        wait ( vif.HRESETn );
         forever begin
             @( posedge vif.HCLK );
-
-            if ( vif.HREADY && vif.HSEL &&
-               ( vif.HTRANS == `HTRANS_NONSEQ ||
-                 vif.HTRANS == `HTRANS_SEQ    )) begin  // Address Phase
-
-                txn = ahb_seq_item :: type_id :: create ("txn");
-                txn.HADDR   = vif.HADDR;
-                txn.HWRITE  = vif.HWRITE;
-                txn.HSIZE   = vif.HSIZE;
-                txn.HBURST  = vif.HBURST;
-                txn.HTRANS  = vif.HTRANS;
-                txn.HSEL    = vif.HSEL;
-                txn.HWDATA  = vif.HWDATA;  // 下一拍才有效，這裡先採樣
-                port.write(txn);
+            if ( vif.HRESETn ) begin
+                if ( vif.HREADY && vif.HSEL &&
+                ( vif.HTRANS == `HTRANS_NONSEQ || vif.HTRANS == `HTRANS_SEQ    )) begin  // Address Phase
+                    txn = ahb_seq_item :: type_id :: create ("txn");
+                    txn.HADDR   = vif.HADDR;
+                    txn.HWRITE  = vif.HWRITE;
+                    txn.HSIZE   = vif.HSIZE;
+                    txn.HBURST  = vif.HBURST;
+                    txn.HTRANS  = vif.HTRANS;
+                    txn.HSEL    = vif.HSEL;
+                    txn.HWDATA  = vif.HWDATA;  // 下一拍才有效，這裡先採樣
+                    port.write(txn);
+                end
             end
         end
     endtask
