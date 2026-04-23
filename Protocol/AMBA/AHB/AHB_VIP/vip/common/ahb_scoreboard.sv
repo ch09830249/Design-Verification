1bc6838210146fe8d578115ad732766a5ef238b6
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-23 17:53:34 +0800

    Update ahb_scoreboard.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_scoreboard.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/common/ahb_scoreboard.sv
@@ -26,13 +26,41 @@ class ahb_scoreboard extends uvm_scoreboard;
         // AHB TXN completes: HTRANS=NONSEQ or SEQ, HREADY=1, HRESP=OKAY
         if ( txn.HSEL && (txn.HTRANS inside {2'b10, 2'b11}) && txn.HREADY && !txn.HRESP ) begin
             if ( txn.HWRITE ) begin  // AHB write
-                mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]] = txn.HWDATA;
+                case ( txn.HSIZE )
+                    `HSIZE_BYTE : begin
+                        mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]] =
+                            { mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]][`D_DATA_WIDTH-1:8],
+                              txn.HWDATA[7:0] };
+                    end
+                    `HSIZE_HALFWORD : begin
+                        mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]] =
+                            { mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]][`D_DATA_WIDTH-1:16],
+                              txn.HWDATA[15:0] };
+                    end
+                    default : begin  // HSIZE_WORD
+                        mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]] = txn.HWDATA;
+                    end
+                endcase
             end else begin  // AHB read
                 exp_data = mem[txn.HADDR[$clog2(`D_MEM_SIZE)-1:0]];
+                case ( txn.HSIZE )
+                    `HSIZE_BYTE     : exp_data = { '0, exp_data[7:0]  };
+                    `HSIZE_HALFWORD : exp_data = { '0, exp_data[15:0] };
+                    default         : exp_data = exp_data;  // HSIZE_WORD
+                endcase
                 if ( exp_data != txn.HRDATA ) begin
-                    `uvm_error(
-                        "MISCOMPARE",
-                        $sformatf("Read addr (0x%h): data = 0x%h while expected 0x%h", txn.HADDR, txn.HRDATA, exp_data)
+                    `uvm_fatal(
+                        "DATA_MISMATCH",
+                        $sformatf("Read addr (0x%h) size=%0h: data = 0x%h while expected 0x%h",
+                                  txn.HADDR, txn.HSIZE, txn.HRDATA, exp_data)
+                    )
+                end 
+                else begin
+                    `uvm_info(
+                        "DATA_MATCH",
+                        $sformatf("Read addr (0x%h) size=%0h: data = 0x%h and expected 0x%h",
+                                txn.HADDR, txn.HSIZE, txn.HRDATA, exp_data),
+                        UVM_LOW
                     )
                 end
             end
