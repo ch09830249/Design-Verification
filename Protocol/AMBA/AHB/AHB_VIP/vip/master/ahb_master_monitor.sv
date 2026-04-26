40d5c3c65843f32c3ffb18492dc313179facaae7
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-26 22:05:42 +0800

    Update ahb_master_monitor.sv

--- Protocol/AMBA/AHB/AHB_VIP/vip/master/ahb_master_monitor.sv
+++ Protocol/AMBA/AHB/AHB_VIP/vip/master/ahb_master_monitor.sv
@@ -4,14 +4,13 @@
 class ahb_master_monitor extends ahb_monitor_base;
     `uvm_component_utils(ahb_master_monitor)
 
-    ahb_seq_item    addr_phase_txn;
+    ahb_seq_item    addr_phase_txn; // previous pending txn
 
     function new ( string name = "ahb_master_monitor", uvm_component parent );
         super.new(name, parent);
     endfunction
 
     virtual task run_phase ( uvm_phase phase );
-        // 等 reset 結束才開始採樣
         wait ( !vif.HRESETn );
         wait ( vif.HRESETn );
 
@@ -19,17 +18,16 @@ class ahb_master_monitor extends ahb_monitor_base;
             @( posedge vif.HCLK );
 
             if ( !vif.HRESETn ) begin
-                // Reset 期間清掉暫存
+                // Clear previous pending txn
                 addr_phase_txn = null;
             end else if ( vif.HREADY ) begin
-
                 // ----------------------------------------
-                // Data Phase — 處理上一個 address phase
+                // Data Phase — handle the previous pending txn
                 // ----------------------------------------
                 if ( addr_phase_txn != null ) begin
                     txn = ahb_seq_item :: type_id :: create ("txn");
 
-                    // Address phase 資訊從 addr_phase_txn 取得
+                    // Get infos of Address phase from addr_phase_txn
                     txn.HADDR       = addr_phase_txn.HADDR;
                     txn.HBURST      = addr_phase_txn.HBURST;
                     txn.HMASTLOCK   = addr_phase_txn.HMASTLOCK;
@@ -39,7 +37,7 @@ class ahb_master_monitor extends ahb_monitor_base;
                     txn.HWRITE      = addr_phase_txn.HWRITE;
                     txn.HSEL        = addr_phase_txn.HSEL;
 
-                    // Data phase 資訊從 vif 直接採樣
+                    // Get infos of Data phase by sampling vif signals
                     txn.HRDATA  = vif.HRDATA;
                     txn.HREADY  = vif.HREADY;
                     txn.HRESP   = vif.HRESP;
@@ -48,12 +46,12 @@ class ahb_master_monitor extends ahb_monitor_base;
                     end else begin
                         txn.HWDATA = '0;
                     end
-                    port.write(txn);
+                    port.write(txn);    // Sending txn to scb and coverage
                     addr_phase_txn = null;
                 end
 
                 // ----------------------------------------
-                // Address Phase — 採樣當前 address phase
+                // Address Phase — Sample signals of address phase
                 // ----------------------------------------
                 if ( vif.HTRANS == `HTRANS_IDLE ) begin
                     addr_phase_txn = null;
@@ -68,11 +66,9 @@ class ahb_master_monitor extends ahb_monitor_base;
                     addr_phase_txn.HWRITE       = vif.HWRITE;
                     addr_phase_txn.HSEL         = vif.HSEL;
                 end
-
             end
         end
     endtask
-
 endclass
 
 `endif
