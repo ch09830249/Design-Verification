9bd4f0c03f232583a7f8cc2d81667469f3c0cbf8
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-27 09:33:39 +0800

    Create apb_vip.f

---
+++ Protocol/AMBA/APB/APB_VIP/apb_vip.f
@@ -0,0 +1,19 @@
+-uvm
+-incdir ./vip/common
+-incdir ./vip/interface
+-incdir ./vip/sva
+-incdir ./vip/base
+-incdir ./vip/master
+-incdir ./vip/slave
+-incdir ./bfm
+-incdir ./seq
+-incdir ./test
+./top/sim_top.sv
+./vip/common/apb_package.svh
+./vip/interface/apb_interface.sv
+./vip/sva/apb_protocol_sva.sv
+./vip/sva/bind_apb_protocol_sva.sv
+./bfm/apb_slave_bfm.sv
+./seq/apb_master_seq.sv
+./seq/apb_slave_seq.sv
+./test/apb_basic_rw_test.sv
