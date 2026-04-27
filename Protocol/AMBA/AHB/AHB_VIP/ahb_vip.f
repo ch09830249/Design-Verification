978c5bb7b113cdceb1fe7c32214e55cd65f23004
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-27 09:32:57 +0800

    Create ahb_vip.f

---
+++ Protocol/AMBA/AHB/AHB_VIP/ahb_vip.f
@@ -0,0 +1,18 @@
+-uvm
+-incdir ./vip/common
+-incdir ./vip/interface
+-incdir ./vip/sva
+-incdir ./vip/base
+-incdir ./vip/master
+-incdir ./vip/slave
+-incdir ./bfm
+-incdir ./seq
+-incdir ./test
+./top/sim_top.sv
+./vip/common/ahb_package.svh
+./vip/interface/ahb_interface.sv
+./vip/sva/ahb_protocol_sva.sv
+./vip/sva/bind_ahb_protocol_sva.sv
+./bfm/ahb_slave_bfm.sv
+./seq/ahb_master_seq.sv
+./test/ahb_basic_rw_test.sv
