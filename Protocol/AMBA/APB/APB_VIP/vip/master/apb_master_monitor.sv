2098024d4d89c94c93aab28f47e0ebd0b910982f
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-29 14:03:08 +0800

    Update apb_master_monitor.sv

--- Protocol/AMBA/APB/APB_VIP/vip/master/apb_master_monitor.sv
+++ Protocol/AMBA/APB/APB_VIP/vip/master/apb_master_monitor.sv
@@ -11,7 +11,7 @@ class apb_master_monitor extends apb_monitor_base;
     virtual task run_phase ( uvm_phase phase );
         forever begin
             @ ( posedge vif.PCLK );
-            if ( vif.PSEL ) begin  // Write whenever PSEL != 0
+            if ( vif.PSEL && vif.PENABLE && vif.PREADY ) begin  // Access phase complete
                 txn = apb_seq_item :: type_id :: create ("txn");
                 // Master Signal
                 txn.PADDR   = vif.PADDR;
