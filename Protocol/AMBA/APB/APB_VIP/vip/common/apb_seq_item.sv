5f9323e7a8f7d92913ac7d5382a9dfc6582e479e
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-29 14:01:34 +0800

    Update apb_seq_item.sv

--- Protocol/AMBA/APB/APB_VIP/vip/common/apb_seq_item.sv
+++ Protocol/AMBA/APB/APB_VIP/vip/common/apb_seq_item.sv
@@ -17,13 +17,13 @@ class apb_seq_item extends uvm_sequence_item;
     bit                             PSLVERR;
 
     `uvm_object_utils_begin(apb_seq_item)
-        `uvm_field_int(PADDR, UVM_ALL_ON)
-        `uvm_field_int(PWRITE, UVM_ALL_ON)
-        `uvm_field_int(PSEL, UVM_ALL_ON)
-        `uvm_field_int(PWDATA, UVM_ALL_ON)
+        `uvm_field_int(PADDR,   UVM_ALL_ON)
+        `uvm_field_int(PWRITE,  UVM_ALL_ON)
+        `uvm_field_int(PSEL,    UVM_ALL_ON)
+        `uvm_field_int(PWDATA,  UVM_ALL_ON)
         `uvm_field_int(PENABLE, UVM_ALL_ON)
-        `uvm_field_int(PREADY, UVM_ALL_ON)
-        `uvm_field_int(PRDATA, UVM_ALL_ON)
+        `uvm_field_int(PREADY,  UVM_ALL_ON)
+        `uvm_field_int(PRDATA,  UVM_ALL_ON)
         `uvm_field_int(PSLVERR, UVM_ALL_ON)
     `uvm_object_utils_end
 
