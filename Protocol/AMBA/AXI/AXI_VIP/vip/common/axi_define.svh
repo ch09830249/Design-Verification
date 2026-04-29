95a70444706bd48534fa6f3afedca1d6da6d3859
Kenny Chang <59005836+ch09830249@users.noreply.github.com>
2026-04-29 09:49:12 +0800

    Update axi_define.svh

--- Protocol/AMBA/AXI/AXI_VIP/vip/common/axi_define.svh
+++ Protocol/AMBA/AXI/AXI_VIP/vip/common/axi_define.svh
@@ -1,12 +1,23 @@
 `ifndef AXI_DEFINE_SVH
 `define AXI_DEFINE_SVH
 
+// ---- Memory parameters ----
 `define D_DATA_WIDTH    32
 `define D_ADDR_WIDTH    32
-`define D_MEM_SIZE      4096   // words
+`define D_MEM_SIZE      4096
 `define D_ID_WIDTH      4
 
-// BRESP / RRESP
+// ---- AWSIZE / ARSIZE ----
+`define ASIZE_BYTE      3'b000
+`define ASIZE_HALFWORD  3'b001
+`define ASIZE_WORD      3'b010
+
+// ---- AWBURST / ARBURST ----
+`define ABURST_FIXED    2'b00
+`define ABURST_INCR     2'b01
+`define ABURST_WRAP     2'b10
+
+// ---- BRESP / RRESP ----
 `define BRESP_OKAY      2'b00
 `define BRESP_EXOKAY    2'b01
 `define BRESP_SLVERR    2'b10
@@ -16,10 +27,5 @@
 `define RRESP_EXOKAY    2'b01
 `define RRESP_SLVERR    2'b10
 `define RRESP_DECERR    2'b11
-
-// AWBURST / ARBURST
-`define BURST_FIXED     2'b00
-`define BURST_INCR      2'b01
-`define BURST_WRAP      2'b10
 
 `endif
