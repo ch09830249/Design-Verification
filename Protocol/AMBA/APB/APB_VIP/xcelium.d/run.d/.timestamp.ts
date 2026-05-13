a570049fe5e624eb53692c9bd86808374b9bc405
ch09830249 <ch09830249@yahoo.com.tw>
2026-05-13 10:32:35 +0800

    Update APB VIP

---
+++ Protocol/AMBA/APB/APB_VIP/xcelium.d/run.d/.timestamp.ts
@@ -0,0 +1,9 @@
+1776235607 /home/users3/kenny780/kenny/uvm/apb_vip/vip/sva/bind_apb_protocol_sva.sv
+1776316613 /home/users3/kenny780/kenny/uvm/apb_vip/top/sim_top.sv
+1776316054 /home/users3/kenny780/kenny/uvm/apb_vip/bfm/apb_slave_bfm.sv
+1777361613 /home/users3/kenny780/kenny/uvm/apb_vip/test/apb_basic_rw_test.sv
+1777361540 /home/users3/kenny780/kenny/uvm/apb_vip/vip/common/apb_package.svh
+1776314861 /home/users3/kenny780/kenny/uvm/apb_vip/seq/apb_slave_seq.sv
+1777363590 /home/users3/kenny780/kenny/uvm/apb_vip/vip/sva/apb_protocol_sva.sv
+1777441948 /home/users3/kenny780/kenny/uvm/apb_vip/seq/apb_master_seq.sv
+1776235750 /home/users3/kenny780/kenny/uvm/apb_vip/vip/interface/apb_interface.sv
---
+++ Protocol/AMBA/APB/APB_VIP/xcelium.d/run.lnx86.23.03.d/worklib_-3.ts
@@ -0,0 +1,2 @@
+1679510835 /home/project/eda/pkgs/cadence/xcelium/v23.03/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
+1679510834 /home/project/eda/pkgs/cadence/xcelium/v23.03/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
